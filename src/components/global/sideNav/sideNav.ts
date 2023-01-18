85d4a5dabae139daa59ad9b9475e22e1565dd993
Brian Patrick <brianthefro@gmail.com>
2023-01-18 12:00:12 -0600

    fix build

--- src/components/global/sideNav/sideNav.ts
+++ src/components/global/sideNav/sideNav.ts
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import SideNavScss from './sideNav.scss';
 import '../../../components/reusable/icon';
