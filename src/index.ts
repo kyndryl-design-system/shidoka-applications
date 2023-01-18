68a0d8bc0cfd1a990db67a9591740c0b6176fde4
Brian Patrick <brianthefro@gmail.com>
2023-01-18 13:55:41 -0600

    index - add side nav exports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { HeaderNav } from './components/global/header/headerNav';
 import { HeaderNavLink } from './components/global/header/headerNavLink';
 import { HeaderFlyouts } from './components/global/header/headerFlyouts';
 import { HeaderFlyout } from './components/global/header/headerFlyout';
+import { SideNav } from './components/global/sideNav/sideNav';
+import { SideNavLink } from './components/global/sideNav/sideNavLink';
 
 export {
   Button,
@@ -14,4 +16,6 @@ export {
   HeaderNavLink,
   HeaderFlyouts,
   HeaderFlyout,
+  SideNav,
+  SideNavLink,
 };
