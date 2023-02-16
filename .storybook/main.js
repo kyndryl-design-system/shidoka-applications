91c3ac5e14f9237d712a2758ed17dbfebb7b5bd8
Brian Patrick <brian.patrick3@kyndryl.com>
2023-02-16 10:32:06 -0600

    storybook - add storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-designs',
+    '@storybook/addon-storysource',
   ],
   framework: {
     name: '@storybook/web-components-webpack5',
