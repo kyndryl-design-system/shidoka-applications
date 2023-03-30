0a269bd46f6995c910ac3a4bd269dc0d237de673
Naga Jyothi Daggumalli <Naga.Jyothi.Daggumalli@ocean.ibm.com>
2023-03-30 14:47:46 -0400

    fixed few things based on comments

--- src/components/global/footer/footer.ts
+++ src/components/global/footer/footer.ts
@@ -2,35 +2,20 @@ import { LitElement, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import { debounce } from '../../../common/helpers/helpers';
 import logo from '../../../assets/svg/bridge-logo-large.svg';
 import FooterScss from './footer.scss';
 
 @customElement('kyn-footer')
 export class Footer extends LitElement {
   static override styles = FooterScss;
-
-  @property({ type: Number })
-  breakpoint = 672;
   
   /** URL for the footer logo link. Should target the application home page. */
   @property({ type: String })
   rootUrl = '/';
-  
-  @property({ type: Number})
-  cprYear = '2023'
-
-  /**
-   * determines the screen breakpoint
-   * @ignore
-   */
-  @state()
-  breakpointHit = false;
 
   override render() {
     const classes = {
       footer: true,
-      'breakpoint-hit': this.breakpointHit
     };
 
     return html`
@@ -39,47 +24,16 @@ export class Footer extends LitElement {
     <div class="footer-cpr">
         <a
           href="${this.rootUrl}"
-          class="logo-link ${this.breakpointHit ? 'breakpoint-hit' : ''}"
+          class="logo-link"
           @click="${(e: Event) => this.handleRootLinkClick(e)}"
         >
           <slot name="logo"> ${unsafeHTML(logo)} </slot>
         </a>
-        <span class="copyright ${this.breakpointHit ? 'breakpoint-hit' : ''}">
-          Copyright &copy; ${this.cprYear} Kyndryl Inc. All rights reserved
+        <span class="copyright">
+          Copyright &copy; ${new Date().getFullYear()} Kyndryl Inc. All rights reserved
         </span>
       </div>
     </footer>`
-  }
-
-  override connectedCallback() {
-    super.connectedCallback();
-
-    this.testBreakpoint();
-    window?.addEventListener(
-      'resize',
-      debounce(() => {
-        this.testBreakpoint();
-      })
-    );
-  }
-
-  override disconnectedCallback() {
-    window?.removeEventListener(
-      'resize',
-      debounce(() => {
-        this.testBreakpoint();
-      })
-    );
-
-    super.disconnectedCallback();
-  }
-
-  private testBreakpoint() {
-    if (window?.innerWidth <= this.breakpoint) {
-      this.breakpointHit = true;
-    } else {
-      this.breakpointHit = false;
-    }
   }
 
   private handleRootLinkClick(e: Event) {
