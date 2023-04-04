a4b9a98f350ab294274daaed5f63404e32a4b7ef
Jonathan Dudzik <jonathan.dudzik@kyndryl.com>
2023-04-04 11:00:27 -0400

    fix: format changes and commit message for auto publish

--- src/components/global/footer/footer.ts
+++ src/components/global/footer/footer.ts
@@ -14,9 +14,7 @@ export class Footer extends LitElement {
   rootUrl = '/';
 
   override render() {
-    const classes = {
-      footer: true,
-    };
+    const classes = { footer: true };
 
     return html` <footer class="${classMap(classes)}">
       <div class="footer-links"><slot></slot></div>
