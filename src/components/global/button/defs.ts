7a3310468894b25c668e365436d6146285aee645
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2023-08-01 21:58:34 +0530

    feat: create button folder

---
+++ src/components/global/button/defs.ts
@@ -0,0 +1,18 @@
+export enum BUTTON_TYPES {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+    TEXT = 'text',
+    TERTIARY = 'tertiary',
+    LINK = 'link',
+}
+
+export enum BUTTON_SIZES {
+    LARGE = 'large',
+    SMALL = 'small',
+}
+  
+export enum BUTTON_ICON_POSITION {
+    CENTER = 'center',
+    LEFT = 'left',
+    RIGHT = 'right',
+}
