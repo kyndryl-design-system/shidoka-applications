b69a5fd3e59be7d175d05ef605b4834e88a46d78
Brian Patrick <brian.patrick3@kyndryl.com>
2023-12-14 12:00:55 -0600

    docs(header): add everything story

--- src/components/global/header/Header.stories.js
+++ src/components/global/header/Header.stories.js
@@ -149,3 +149,88 @@ export const WithPanel = {
     </kyn-header>
   `,
 };
+
+export const WithEverything = {
+  args,
+  render: (args) => html`
+    <kyn-header
+      rootUrl=${args.rootUrl}
+      appTitle=${args.appTitle}
+      breakpoint=${args.breakpoint}
+      ?divider=${args.divider}
+    >
+      <kyn-header-panel slot="left" heading="Panel Heading">
+        <span slot="button">${unsafeHTML(switcherIcon)}</span>
+
+        <kyn-header-panel-link href="javascript:void(0)">
+          <kd-icon .icon=${userAvatarIcon}></kd-icon>
+          Link 1
+        </kyn-header-panel-link>
+        <kyn-header-panel-link href="javascript:void(0)">
+          <kd-icon .icon=${userAvatarIcon}></kd-icon>
+          Link 2
+        </kyn-header-panel-link>
+        <kyn-header-panel-link href="javascript:void(0)">
+          <kd-icon .icon=${userAvatarIcon}></kd-icon>
+          Link 3
+        </kyn-header-panel-link>
+      </kyn-header-panel>
+
+      <kyn-header-nav>
+        <kyn-header-link href="javascript:void(0)"> Link 1 </kyn-header-link>
+        <kyn-header-link href="javascript:void(0)" isActive>
+          Link 2
+        </kyn-header-link>
+        <kyn-header-link href="javascript:void(0)">
+          <kd-icon .icon=${helpIcon}></kd-icon>
+          Link 3
+
+          <kyn-header-link slot="links" href="javascript:void(0)" divider>
+            Sub Link # 1
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            Sub Link 2
+          </kyn-header-link>
+        </kyn-header-link>
+      </kyn-header-nav>
+
+      <kyn-header-flyouts>
+        <kyn-header-flyout>
+          <span slot="button">Sign in</span>
+
+          <div>
+            <kyn-header-link href="javascript:void(0)"> Login </kyn-header-link>
+            <kyn-header-link href="javascript:void(0)">
+              Sign up
+            </kyn-header-link>
+          </div>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout>
+          <span
+            slot="button"
+            style="display: flex; align-items: center; gap: 6px;"
+          >
+            <kd-icon .icon=${helpIcon}></kd-icon>
+            Support
+          </span>
+
+          <div>
+            <kyn-header-link href="javascript:void(0)">
+              Example 1
+            </kyn-header-link>
+            <kyn-header-link href="javascript:void(0)">
+              Example 2
+            </kyn-header-link>
+          </div>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout assistiveText="My Account" hideArrow>
+          <kyn-header-avatar initials="KB" slot="button"></kyn-header-avatar>
+
+          <kyn-header-link href="javascript:void(0)"> Logout </kyn-header-link>
+        </kyn-header-flyout>
+      </kyn-header-flyouts>
+    </kyn-header>
+  `,
+};
