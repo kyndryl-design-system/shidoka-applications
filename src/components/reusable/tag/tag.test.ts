4460354ac708a01b9d281e0b24c5af587c729c47
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2024-02-19 20:32:17 +0530

    fix: fix typo in test file

--- src/components/reusable/tag/tag.test.ts
+++ src/components/reusable/tag/tag.test.ts
@@ -2,14 +2,14 @@ import { Tag, TagGroup } from '../../../../dist/index.js';
 
 import { assert } from '@open-wc/testing';
 
-suite('kyn-checkbox', () => {
+suite('kyn-tag', () => {
   test('is defined', () => {
     const el = document.createElement('kyn-tag');
     assert.instanceOf(el, Tag);
   });
 });
 
-suite('kyn-checkbox-group', () => {
+suite('kyn-tag-group', () => {
   test('is defined', () => {
     const el = document.createElement('kyn-tag-group');
     assert.instanceOf(el, TagGroup);
