a001f15740b3e613bc55a3f9e80236d96fba4f2c
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2024-03-22 21:28:27 +0530

    chore(card): remove unused imports

--- src/components/reusable/card/card.sample.ts
+++ src/components/reusable/card/card.sample.ts
@@ -1,5 +1,5 @@
 import { html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 import CardSampleScss from './cardSample.scss';
 
 /**  Sample Lit component to show card pattern. */
