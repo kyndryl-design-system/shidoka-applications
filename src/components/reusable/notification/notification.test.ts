137a132d277b21c11486abfcd42bc2f3363330a8
maulik-thaker1 <Maulik.Thaker1@kyndryl.com>
2024-03-30 13:16:44 +0530

    fix: Update test cases of notification

--- src/components/reusable/notification/notification.test.ts
+++ src/components/reusable/notification/notification.test.ts
@@ -1,13 +1,6 @@
-import { NotificationPanel, Notification } from '../../../../dist/index.js';
+import { Notification } from '../../../../dist/index.js';
 
 import { assert } from '@open-wc/testing';
-
-suite('kyn-notification-panel', () => {
-  test('is defined', () => {
-    const el = document.createElement('kyn-notification-panel');
-    assert.instanceOf(el, NotificationPanel);
-  });
-});
 
 suite('kyn-notification', () => {
   test('is defined', () => {
