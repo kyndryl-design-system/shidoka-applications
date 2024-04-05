51b7b6a85d53135f6e4881e5fef8ddde65d5a370
maulik-thaker1 <maulik.thaker1@kyndryl.com>
2024-04-05 15:54:49 +0530

    fix(notification): removed un used imports

--- src/components/reusable/card/card.ts
+++ src/components/reusable/card/card.ts
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 
 import CardScss from './card.scss';
