dcb68bbc0cca96c03741ef48327e868b293321f0
Brian Patrick <brian.patrick3@kyndryl.com>
2024-04-10 15:27:09 -0500

    test: delete old unit test files

--- src/components/reusable/daterangepicker/daterangepicker.test.ts
+++
@@ -1,9 +0,0 @@
-import { DateRangePicker } from '../../../../dist/index.js';
-import { assert } from '@open-wc/testing';
-
-suite('kyn-date-range-picker', () => {
-  test('is defined', () => {
-    const el = document.createElement('kyn-date-range-picker');
-    assert.instanceOf(el, DateRangePicker);
-  });
-});
