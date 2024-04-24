3a2f561c1a0f0d15500f277edf4d59fd55e27032
Maulik Thaker <140634142+maulik-thaker1@users.noreply.github.com>
2024-04-25 00:21:40 +0530

    docs(vital-card): Added vital card pattern story (#155)
    * feat(vital-card): Added vital card pattern
    
    * fix(vital-card): typo
    
    * fix(vital-card): issue of WCAG and typo
    
    * fix(vital-card): fix button WCAG issue
    
    * fix(vital-card): A11y bug for color contrast ratio
    
    * fix(vital-card): un used import
    
    * style(vital-card): Added padding to mobile version component
    
    * feat(vital-card): change button to link as per UX suggesion
    
    * feat(vital-card): Change stories for vital card
    
    * fix(vital-card): As per review comments
    
    * style(vital-card): remove unwanted styles

---
+++ src/stories/vitalCard.stories.js
@@ -0,0 +1,23 @@
+import { html } from 'lit';
+import '@kyndryl-design-system/shidoka-foundation/css/grid.css';
+import '@kyndryl-design-system/shidoka-foundation/components/card';
+
+import './sampleCardComponents/vitalCard.sample.ts';
+
+export default {
+  title: 'Patterns/Vital Card',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/CQuDZEeLiuGiALvCWjAKlu/Applications---Component-Library?type=design&node-id=9941-18758&mode=design&t=N5Lw7kAKTAjoCa0x-0',
+    },
+  },
+};
+
+export const Default = {
+  render: () => {
+    return html`<kd-card type="normal">
+      <vital-card-sample-component></vital-card-sample-component>
+    </kd-card>`;
+  },
+};
