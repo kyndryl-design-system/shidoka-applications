94cb1addb2a9c6c77bbe0f20b3203239e7a35f84
maulik-thaker1 <maulik.thaker1@kyndryl.com>
2024-04-24 17:46:17 +0530

    feat(vital-card): Change stories for vital card

--- src/stories/sampleCardComponents/vitalCard.sample.ts
+++ src/stories/sampleCardComponents/vitalCard.sample.ts
@@ -3,6 +3,7 @@ import { customElement } from 'lit/decorators.js';
 import vitalCardScss from './vitalCard.scss';
 import '@kyndryl-design-system/shidoka-foundation/components/icon';
 import '@kyndryl-design-system/shidoka-foundation/components/link';
+import '@kyndryl-design-system/shidoka-foundation/components/button';
 
 import chevronRightIcon from '@carbon/icons/es/chevron--right/20';
 
@@ -15,25 +16,40 @@ export class VitalCardSampleComponent extends LitElement {
       <div>
         <div class="vital-card-title-label">Title</div>
         <div class="vital-card-content-wrapper">
-          <h1 class="vital-card-title">9,999.99k</h1>
-          <h2 class="vital-card-category-text">Category</h2>
-          <h2 class="vital-card-subcategory-text">Subcategory</h2>
-          <div class="vital-card-cta">
-            <slot></slot>
-            <kd-link
-              class="vital-card-link"
-              standalone
-              href="#"
-              @on-click=${(e: Event) => e.preventDefault()}
-            >
-              CTA Title
-              <kd-icon
-                slot="icon"
-                sizeoverride="16"
-                .icon=${chevronRightIcon}
-              ></kd-icon>
-            </kd-link>
+          <div class="vital-card-mobile-wrapper-subdiv">
+            <div class="vital-card-title-div">
+              <h1 class="vital-card-title">9,999.99k</h1>
+            </div>
+            <div class="vital-card-cat-subcat-text">
+              <h2 class="vital-card-category-text">Category</h2>
+              <h2 class="vital-card-subcategory-text">Subcategory</h2>
+            </div>
           </div>
+
+          <kd-link
+            class="vital-card-link"
+            standalone
+            href="#"
+            @on-click=${(e: Event) => e.preventDefault()}
+          >
+            CTA Title
+            <kd-icon
+              slot="icon"
+              sizeoverride="16"
+              .icon=${chevronRightIcon}
+            ></kd-icon>
+          </kd-link>
+
+          <kd-button
+            class="vital-card-icon-btn"
+            iconposition="center"
+            kind="tertiary"
+            size="small"
+            description="cta button"
+            @click=${(e: Event) => e.preventDefault()}
+          >
+            <kd-icon slot="icon" .icon=${chevronRightIcon}></kd-icon>
+          </kd-button>
         </div>
       </div>
     `;
--- src/stories/vitalCard.stories.js
+++ src/stories/vitalCard.stories.js
@@ -3,7 +3,7 @@ import '@kyndryl-design-system/shidoka-foundation/css/grid.css';
 import '@kyndryl-design-system/shidoka-foundation/components/card';
 
 import './sampleCardComponents/vitalCard.sample.ts';
-import './sampleCardComponents/vitalCard.mobile.sample.ts';
+//import './sampleCardComponents/vitalCard.mobile.sample.ts';
 
 export default {
   title: 'Patterns/Vital Card',
@@ -31,18 +31,18 @@ export const Clickable = {
   },
 };
 
-export const DefaultMobile = {
-  render: () => {
-    return html`<kd-card type="normal" href="" target="" rel="">
-      <vital-card-mobile-sample-component></vital-card-mobile-sample-component
-    ></kd-card>`;
-  },
-};
+// export const DefaultMobile = {
+//   render: () => {
+//     return html`<kd-card type="normal" href="" target="" rel="">
+//       <vital-card-mobile-sample-component></vital-card-mobile-sample-component
+//     ></kd-card>`;
+//   },
+// };
 
-export const MobileClickable = {
-  render: () => {
-    return html`<kd-card type="clickable" href="" target="" rel="">
-      <vital-card-mobile-sample-component></vital-card-mobile-sample-component
-    ></kd-card>`;
-  },
-};
+// export const MobileClickable = {
+//   render: () => {
+//     return html`<kd-card type="clickable" href="" target="" rel="">
+//       <vital-card-mobile-sample-component></vital-card-mobile-sample-component
+//     ></kd-card>`;
+//   },
+// };
