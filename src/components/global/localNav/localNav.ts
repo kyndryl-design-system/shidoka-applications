94a141a614b9770bba99fe7702fa9f1df542b95a
Brian Patrick <brian.patrick3@kyndryl.com>
2024-05-01 10:27:30 -0500

    fix(local-nav): restore pinned, add hover intent

--- src/components/global/localNav/localNav.ts
+++ src/components/global/localNav/localNav.ts
@@ -10,6 +10,8 @@ import LocalNavScss from './localNav.scss';
 import '@kyndryl-design-system/shidoka-foundation/components/icon';
 
 import arrowIcon from '@carbon/icons/es/chevron--down/16';
+import openIcon from '@carbon/icons/es/side-panel--open/24';
+import closeIcon from '@carbon/icons/es/side-panel--close/24';
 
 /**
  * The global Side Navigation component.
@@ -19,6 +21,18 @@ import arrowIcon from '@carbon/icons/es/chevron--down/16';
 @customElement('kyn-local-nav')
 export class LocalNav extends LitElement {
   static override styles = LocalNavScss;
+
+  /** Local nav pinned state. */
+  @property({ type: Boolean })
+  pinned = false;
+
+  /** Pin open button assistive text. */
+  @property({ type: String })
+  pinText = 'Pin';
+
+  /** Unpin button assistive text. */
+  @property({ type: String })
+  unpinText = 'Unpin';
 
   /** Menu toggle button assistive text. */
   @property({ type: Object })
@@ -46,16 +60,21 @@ export class LocalNav extends LitElement {
   @queryAssignedElements({ selector: 'kyn-local-nav-link' })
   _navLinks!: any;
 
-  /** Timeout function to delay modal close.
+  /** Timeout function to delay flyout open.
+   * @internal
+   */
+  _enterTimer: any;
+
+  /** Timeout function to delay flyout close.
    * @internal
    */
   @state()
-  timer: any;
+  _leaveTimer: any;
 
   override render() {
     return html`
       <nav
-        class=${classMap({ 'nav--expanded': this._expanded })}
+        class=${classMap({ 'nav--expanded': this._expanded || this.pinned })}
         @pointerleave=${(e: PointerEvent) => this.handlePointerLeave(e)}
         @pointerenter=${(e: PointerEvent) => this.handlePointerEnter(e)}
       >
@@ -74,39 +93,57 @@ export class LocalNav extends LitElement {
         <div class="links">
           <slot @slotchange=${this.handleSlotChange}></slot>
         </div>
+
+        <div class="toggle-container">
+          <button
+            class="nav-toggle"
+            @click=${(e: Event) => this._handleNavToggle(e)}
+            title="${this.pinned ? this.unpinText : this.pinText}"
+            aria-label="${this.pinned ? this.unpinText : this.pinText}"
+          >
+            <kd-icon .icon=${this.pinned ? closeIcon : openIcon}></kd-icon>
+          </button>
+        </div>
       </nav>
+
       <div class="overlay"></div>
     `;
   }
 
   private _handleNavToggle(e: Event) {
-    this._expanded = !this._expanded;
+    this.pinned = !this.pinned;
 
     const event = new CustomEvent('on-toggle', {
-      detail: { open: this._expanded, pinned: this._expanded, origEvent: e },
+      detail: { pinned: this.pinned, origEvent: e },
     });
     this.dispatchEvent(event);
   }
 
   private handlePointerEnter(e: PointerEvent) {
     if (e.pointerType === 'mouse') {
-      clearTimeout(this.timer);
-      this._expanded = true;
+      clearTimeout(this._leaveTimer);
+
+      this._enterTimer = setTimeout(() => {
+        this._expanded = true;
+        clearTimeout(this._leaveTimer);
+      }, 150);
     }
   }
 
   private handlePointerLeave(e: PointerEvent) {
-    if (e.pointerType === 'mouse' && document.activeElement !== this) {
-      this.timer = setTimeout(() => {
+    if (e.pointerType === 'mouse') {
+      clearTimeout(this._enterTimer);
+
+      this._leaveTimer = setTimeout(() => {
         this._expanded = false;
-        clearTimeout(this.timer);
-      }, 100);
+        clearTimeout(this._leaveTimer);
+      }, 150);
     }
   }
 
   private _updateChildren() {
     this._navLinks.forEach((link: any) => {
-      link._navExpanded = this._expanded;
+      link._navExpanded = this._expanded || this.pinned;
     });
   }
 
@@ -120,7 +157,7 @@ export class LocalNav extends LitElement {
   }
 
   override willUpdate(changedProps: any) {
-    if (changedProps.has('_expanded')) {
+    if (changedProps.has('_expanded') || changedProps.has('pinned')) {
       this._updateChildren();
     }
   }
