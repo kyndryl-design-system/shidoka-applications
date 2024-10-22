37b71edb590a45df7a5b6aacb18844fa84d01d78
Will Barbee <will.clayton.barbee@gmail.com>
2024-10-22 09:55:51 -0500

    feat(custom-date-picker): updated custom date, time, date range pickers w flatpickr (#242)

--- src/components/reusable/timepicker/timepicker.stories.js
+++ src/components/reusable/timepicker/timepicker.stories.js
@@ -1,76 +1,113 @@
 import { html } from 'lit';
 import './index';
 import { action } from '@storybook/addon-actions';
-import { ifDefined } from 'lit/directives/if-defined.js';
-import '@kyndryl-design-system/shidoka-foundation/components/icon';
+import { useEffect } from '@storybook/addons';
 
 export default {
   title: 'Components/Timepicker',
   component: 'kyn-time-picker',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/s9VKYHFn1GncFyxd5l19nU/1.11-Amsterdam?node-id=6086-1559&node-type=canvas&m=dev',
+    },
+  },
   argTypes: {
     size: {
       options: ['sm', 'md', 'lg'],
       control: { type: 'select' },
     },
-    minTime: {
-      control: { type: 'text' },
-    },
-    maxTime: {
-      control: { type: 'text' },
-    },
-    step: {
-      control: { type: 'text' },
-    },
+    label: { control: { type: 'text' } },
+    locale: { control: { type: 'text' } },
+    minTime: { control: { type: 'text' } },
+    maxTime: { control: { type: 'text' } },
+    defaultDate: { control: { type: 'text' } },
+    defaultHour: { control: { type: 'number' } },
+    defaultMinute: { control: { type: 'number' } },
+    invalidText: { control: { type: 'text' } },
+    defaultErrorMessage: { control: { type: 'text' } },
+    twentyFourHourFormat: { control: { type: 'boolean' } },
   },
 };
 
-const args = {
-  unnamed: 'Time',
-  size: 'md',
-  name: 'timepicker',
-  value: '',
-  caption: '',
+const disconnectFlatpickr = () => {
+  const calendarElements = document.querySelectorAll('.flatpickr-calendar');
+  calendarElements.forEach((calendar) => calendar.remove());
+};
+
+const Template = (args) => {
+  useEffect(() => {
+    return () => {
+      disconnectFlatpickr();
+    };
+  }, []);
+
+  return html`
+    <kyn-time-picker
+      .name="${args.name}"
+      .label="${args.label}"
+      .locale="${args.locale}"
+      ?required="${args.required}"
+      .size="${args.size}"
+      .value="${args.value}"
+      .warnText="${args.warnText}"
+      .invalidText="${args.invalidText}"
+      .caption="${args.caption}"
+      .defaultDate="${args.defaultDate}"
+      .defaultHour="${args.defaultHour}"
+      .defaultMinute="${args.defaultMinute}"
+      .defaultErrorMessage="${args.defaultErrorMessage}"
+      .minTime="${args.minTime}"
+      .maxTime="${args.maxTime}"
+      .errorAriaLabel="${args.errorAriaLabel}"
+      .errorTitle="${args.errorTitle}"
+      .warningAriaLabel="${args.warningAriaLabel}"
+      .warningTitle="${args.warningTitle}"
+      ?timepickerDisabled="${args.timepickerDisabled}"
+      ?twentyFourHourFormat="${args.twentyFourHourFormat}"
+      @on-change=${(e) => action(e.type)(e)}
+    >
+    </kyn-time-picker>
+  `;
+};
+
+export const DefaultTimePicker = Template.bind({});
+DefaultTimePicker.args = {
+  name: 'default-timepicker',
+  locale: 'en',
   required: false,
-  disabled: false,
-  invalidText: '',
+  size: 'md',
+  value: null,
   warnText: '',
-  minTime: undefined,
-  maxTime: undefined,
-  step: undefined,
-  textStrings: {
-    requiredText: 'Required',
-  },
+  invalidText: '',
+  caption: '',
+  defaultDate: '',
+  defaultErrorMessage: 'A time value is required',
+  minTime: '',
+  maxTime: '',
+  errorAriaLabel: 'Error message icon',
+  errorTitle: '',
+  warningAriaLabel: '',
+  warningTitle: '',
+  timepickerDisabled: false,
+  label: 'Timepicker',
 };
+DefaultTimePicker.storyName = 'Default (12H)';
 
-export const TimePicker = {
-  args,
-  render: (args) => {
-    return html`
-      <kyn-time-picker
-        size=${args.size}
-        name=${args.name}
-        value=${args.value}
-        caption=${args.caption}
-        ?required=${args.required}
-        ?disabled=${args.disabled}
-        invalidText=${args.invalidText}
-        warnText=${args.warnText}
-        .textStrings=${args.textStrings}
-        minTime=${ifDefined(args.minTime)}
-        maxTime=${ifDefined(args.maxTime)}
-        step=${ifDefined(args.step)}
-        @on-input=${(e) => action(e.type)(e)}
-        @keydown=${(e) => e.stopPropagation()}
-      >
-        ${args.unnamed}
-      </kyn-time-picker>
-    `;
-  },
+export const TimePickerTwentyFourHour = Template.bind({});
+TimePickerTwentyFourHour.args = {
+  ...DefaultTimePicker.args,
+  locale: 'en',
+  twentyFourHourFormat: true,
+  label: 'Timepicker (24H)',
 };
+TimePickerTwentyFourHour.storyName = 'Timepicker (24H)';
 
-TimePicker.parameters = {
-  design: {
-    type: 'figma',
-    url: 'https://www.figma.com/file/6AovH7Iay9Y7BkpoL5975s/Component-Library-for-Dev?node-id=508%3A142377&mode=dev',
-  },
+export const TimePickerAltLanguage = Template.bind({});
+TimePickerAltLanguage.args = {
+  ...DefaultTimePicker.args,
+  locale: 'ja',
+  twentyFourHourFormat: false,
+  label: 'Non-English 12H Example (Japanese locale)',
 };
+TimePickerAltLanguage.storyName = 'Japanese Locale Example';
