7b30e7773afaec3cd273f24ec2112d0f7666f57b
Brian Patrick <brian.patrick3@kyndryl.com>
2024-11-15 09:53:57 -0600

    chore(card): fix build

--- src/components/reusable/card/card.stories.js
+++ src/components/reusable/card/card.stories.js
@@ -1,13 +1,14 @@
 import { html } from 'lit';
-import '@kyndryl-design-system/shidoka-foundation/css/grid.css';
+// import '@kyndryl-design-system/shidoka-foundation/css/grid.css';
+import '@kyndryl-design-system/shidoka-foundation/components/card';
 import './index';
-import { action } from '@storybook/addon-actions';
-import './card.sample';
-import './card.content.sample';
+// import { action } from '@storybook/addon-actions';
+// import './card.sample';
+// import './card.content.sample';
 
 export default {
   title: 'Components/Card - Deprecated',
-  component: 'kyn-card',
+  component: 'kd-card',
   argTypes: {
     type: {
       options: ['normal', 'clickable'],
@@ -36,7 +37,7 @@ export const Simple = {
   },
   render: (args) => {
     return html`
-      <kyn-card
+      <kd-card
         type=${args.type}
         href=${args.href}
         target=${args.target}
@@ -50,122 +51,122 @@ export const Simple = {
             of Amazon EC2 instances based on demand.
           </div>
         </sample-card-component>
-      </kyn-card>
+      </kd-card>
     `;
   },
 };
 
-export const WithOtherContents = {
-  args: Simple.args,
-  render: (args) => {
-    return html`<kyn-card
-      type=${args.type}
-      href=${args.href}
-      target=${args.target}
-      rel=${args.rel}
-      ?hideBorder=${args.hideBorder}
-    >
-      <sample-card-content-component></sample-card-content-component>
-    </kyn-card>`;
-  },
-};
+// export const WithOtherContents = {
+//   args: Simple.args,
+//   render: (args) => {
+//     return html`<kyn-card
+//       type=${args.type}
+//       href=${args.href}
+//       target=${args.target}
+//       rel=${args.rel}
+//       ?hideBorder=${args.hideBorder}
+//     >
+//       <sample-card-content-component></sample-card-content-component>
+//     </kyn-card>`;
+//   },
+// };
 
-export const Clickable = {
-  args: {
-    type: 'clickable',
-    href: 'https://www.kyndryl.com',
-    rel: 'noopener',
-    target: '_blank',
-    hideBorder: false,
-  },
-  render: (args) => {
-    return html`
-      <kyn-card
-        type=${args.type}
-        href=${args.href}
-        target=${args.target}
-        rel=${args.rel}
-        ?hideBorder=${args.hideBorder}
-        @on-card-click=${(e) => action(e.type)(e)}
-      >
-        <sample-card-component>
-          <div slot="title">This is a card title</div>
-          <div slot="description">
-            Amazon EC2 Auto Scaling ensures that your application always has the
-            right amount of compute capacity by dynamically adjusting the number
-            of Amazon EC2 instances based on demand.
-          </div>
-        </sample-card-component>
-      </kyn-card>
-    `;
-  },
-};
+// export const Clickable = {
+//   args: {
+//     type: 'clickable',
+//     href: 'https://www.kyndryl.com',
+//     rel: 'noopener',
+//     target: '_blank',
+//     hideBorder: false,
+//   },
+//   render: (args) => {
+//     return html`
+//       <kyn-card
+//         type=${args.type}
+//         href=${args.href}
+//         target=${args.target}
+//         rel=${args.rel}
+//         ?hideBorder=${args.hideBorder}
+//         @on-card-click=${(e) => action(e.type)(e)}
+//       >
+//         <sample-card-component>
+//           <div slot="title">This is a card title</div>
+//           <div slot="description">
+//             Amazon EC2 Auto Scaling ensures that your application always has the
+//             right amount of compute capacity by dynamically adjusting the number
+//             of Amazon EC2 instances based on demand.
+//           </div>
+//         </sample-card-component>
+//       </kyn-card>
+//     `;
+//   },
+// };
 
-export const InsideGrid = {
-  render: () => {
-    return html`
-      <div class="kd-grid">
-        <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
-          <kyn-card style="width:100%;height:100%;">
-            <sample-card-component>
-              <div slot="title">This is a card title</div>
-              <div slot="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris.
-              </div>
-            </sample-card-component>
-          </kyn-card>
-        </div>
-        <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
-          <kyn-card style="width:100%;height:100%;">
-            <sample-card-component>
-              <div slot="title">This is a card title</div>
-              <div slot="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum
-              </div>
-            </sample-card-component>
-          </kyn-card>
-        </div>
-        <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
-          <kyn-card style="width:100%;height:100%;">
-            <sample-card-component>
-              <div slot="title">This is a card title</div>
-              <div slot="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor.
-              </div>
-            </sample-card-component>
-          </kyn-card>
-        </div>
-        <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
-          <kyn-card style="width:100%;height:100%;">
-            <sample-card-component>
-              <div slot="title">This is a card title</div>
-              <div slot="description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
-              </div>
-            </sample-card-component>
-          </kyn-card>
-        </div>
-      </div>
-    `;
-  },
-};
+// export const InsideGrid = {
+//   render: () => {
+//     return html`
+//       <div class="kd-grid">
+//         <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
+//           <kyn-card style="width:100%;height:100%;">
+//             <sample-card-component>
+//               <div slot="title">This is a card title</div>
+//               <div slot="description">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//                 enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+//               </div>
+//             </sample-card-component>
+//           </kyn-card>
+//         </div>
+//         <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
+//           <kyn-card style="width:100%;height:100%;">
+//             <sample-card-component>
+//               <div slot="title">This is a card title</div>
+//               <div slot="description">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+//                 in reprehenderit in voluptate velit esse cillum
+//               </div>
+//             </sample-card-component>
+//           </kyn-card>
+//         </div>
+//         <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
+//           <kyn-card style="width:100%;height:100%;">
+//             <sample-card-component>
+//               <div slot="title">This is a card title</div>
+//               <div slot="description">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor.
+//               </div>
+//             </sample-card-component>
+//           </kyn-card>
+//         </div>
+//         <div class="kd-grid__col--sm-2 kd-grid__col--md-4 kd-grid__col--lg-3">
+//           <kyn-card style="width:100%;height:100%;">
+//             <sample-card-component>
+//               <div slot="title">This is a card title</div>
+//               <div slot="description">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+//                 in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//                 nulla pariatur.
+//               </div>
+//             </sample-card-component>
+//           </kyn-card>
+//         </div>
+//       </div>
+//     `;
+//   },
+// };
 
 export const VitalCardSkeleton = {
   args: { ...Simple.args, lines: 1, thumbnailVisible: true },
   render: (args) => {
-    return html`<kyn-card
+    return html`<kd-card
       type=${args.type}
       href=${args.href}
       target=${args.target}
@@ -173,7 +174,7 @@ export const VitalCardSkeleton = {
       ?hideBorder=${args.hideBorder}
     >
       <kyn-vital-card-skeleton .lines=${args.lines}></kyn-vital-card-skeleton>
-    </kyn-card>`;
+    </kd-card>`;
   },
 };
 
@@ -186,27 +187,27 @@ export const InformationalCardSkeleton = {
       target=${args.target}
       rel=${args.rel}
       ?hideBorder=${args.hideBorder}
-      ><kyn-info-card-skeleton
+      ><kd-info-card-skeleton
         .lines=${args.lines}
         ?thumbnailVisible=${args.thumbnailVisible}
       ></kyn-info-card-skeleton
-    ></kyn-card>`;
+    ></kd-card>`;
   },
 };
 
 export const InformationalCardSkeletonWithThumbnail = {
   args: { ...Simple.args, lines: 2, thumbnailVisible: true },
   render: (args) => {
-    return html` <kyn-card
+    return html` <kd-card
       type=${args.type}
       href=${args.href}
       target=${args.target}
       rel=${args.rel}
       ?hideBorder=${args.hideBorder}
-      ><kyn-info-card-skeleton
+      ><kd-info-card-skeleton
         .lines=${args.lines}
         ?thumbnailVisible=${args.thumbnailVisible}
       ></kyn-info-card-skeleton
-    ></kyn-card>`;
+    ></kd-card>`;
   },
 };
