68876be3a5417ceaeab8b3f29eacfc5cdd065b46
wbarbee <will.clayton.barbee@gmail.com>
2024-12-16 10:13:56 -0600

    fix: bug 2044199 checkbox, checkboxGroup token updates

--- src/components/reusable/checkbox/checkboxGroup.ts
+++ src/components/reusable/checkbox/checkboxGroup.ts
@@ -7,7 +7,7 @@ import CheckboxGroupScss from './checkboxGroup.scss';
 
 import '../textInput';
 import './checkbox';
-import errorIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/warning-filled.svg';
+import errorIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/information-filled.svg';
 
 const _defaultTextStrings = {
   selectAll: 'Select all',
