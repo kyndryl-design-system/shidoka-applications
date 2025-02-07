9c6be40f13046f693d9f3b92975e0e187700a5f8
wbarbee <will.clayton.barbee@gmail.com>
2025-02-07 09:39:07 -0600

    fix: page.evaluate async

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -6,14 +6,27 @@ const { getStoryContext } = require('@storybook/test-runner');
  * to learn more about the test-runner hooks API.
  */
 module.exports = {
-  async preVisit(page, context) {
+  async preVisit(page) {
     await injectAxe(page);
+    await page.evaluate(() => {
+      window.__axeRunning = false;
+    });
   },
+
   async postVisit(page, context) {
     const storyContext = await getStoryContext(page, context);
 
     // run accessibility tests if not disabled
     if (!storyContext.parameters?.a11y?.disable) {
+      if (await page.evaluate(() => window.__axeRunning)) {
+        console.warn('Skipping... Axe is already running');
+        return;
+      }
+
+      await page.evaluate(() => {
+        window.__axeRunning = true;
+      });
+
       const Options = {
         detailedReport: true,
         detailedReportOptions: {
@@ -24,49 +37,16 @@ module.exports = {
       // get custom rules, convert from array to object structure, inject to options
       const Rules = storyContext.parameters?.a11y?.config?.rules;
       if (Rules) {
-        // const RulesObj = {};
-        // Rules.forEach((rule) => {
-        //   RulesObj[rule.id] = { enabled: rule.enabled };
-        // });
-
-        // Options.axeOptions = {
-        //   rules: RulesObj,
-        // };
-
         await configureAxe(page, {
           rules: Rules,
         });
       }
 
       await checkA11y(page, '#storybook-root', Options);
+
+      await page.evaluate(() => {
+        window.__axeRunning = false;
+      });
     }
-
-    // // run snapshot tests
-    // // get story HTML
-    // let html = await page.evaluate(
-    //   "document.querySelector('#root-inner').innerHTML"
-    // );
-
-    // // filter out stories that don't use components
-    // // if (context.title.toLowerCase().includes('components/')) {
-    // // const outerHtml = await page.evaluate(
-    // //   "Array.prototype.slice.call(document.querySelectorAll('*')).find((el) => el.tagName.startsWith('KD-')).outerHTML"
-    // // );
-    // // const shadowRootHtml = await page.evaluate(
-    // //   "Array.prototype.slice.call(document.querySelectorAll('*')).find((el) => el.tagName.startsWith('KD-')).shadowRoot.innerHTML"
-    // // );
-    // // console.log(shadowRootHtml);
-
-    // // get HTML from every element with a shadow root
-    // html += await page.evaluate(`
-    //   let html = '';
-    //   Array.prototype.slice.call(document.querySelectorAll('*'))
-    //     .filter((el) => el.shadowRoot)
-    //     .forEach((el) => html += el.shadowRoot.innerHTML);
-    //   html;
-    // `);
-    // // }
-
-    // expect(html).toMatchSnapshot();
   },
 };
