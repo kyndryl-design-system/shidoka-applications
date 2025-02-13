028d0a82b66638181a2c07dd176f607e5e4508bb
Jaideep M V <107jaideep@gmail.com>
2025-02-13 22:50:55 +0530

    fix: story optimisation

--- src/components/reusable/textArea/textArea.stories.js
+++ src/components/reusable/textArea/textArea.stories.js
@@ -16,90 +16,59 @@ export default {
   },
 };
 
-export const TextArea = {
-  args: {
-    label: 'Label',
-    name: 'text-area-1',
-    value: '',
-    placeholder: '',
-    caption: '',
-    required: false,
-    disabled: false,
-    invalidText: '',
-    minLength: undefined,
-    maxLength: undefined,
-    rows: undefined,
-    textStrings: {
-      requiredText: 'Required',
-      errorText: 'Error',
-    },
-  },
-  render: (args) => {
-    return html`
-      <kyn-text-area
-        name=${args.name}
-        value=${args.value}
-        placeholder=${args.placeholder}
-        caption=${args.caption}
-        ?required=${args.required}
-        ?disabled=${args.disabled}
-        invalidText=${args.invalidText}
-        .textStrings=${args.textStrings}
-        minLength=${ifDefined(args.minLength)}
-        maxLength=${ifDefined(args.maxLength)}
-        rows=${args.rows}
-        @on-input=${(e) => action(e.type)(e)}
-        @keydown=${(e) => e.stopPropagation()}
-        label=${args.label}
-      >
-      </kyn-text-area>
-    `;
+const args = {
+  label: 'Label',
+  name: 'text-area-1',
+  value: '',
+  placeholder: '',
+  caption: '',
+  required: false,
+  disabled: false,
+  invalidText: '',
+  minLength: undefined,
+  maxLength: undefined,
+  rows: undefined,
+  textStrings: {
+    requiredText: 'Required',
+    errorText: 'Error',
   },
 };
 
-export const AIConnected = {
-  args: {
-    label: 'Label',
-    name: 'text-area-1',
-    value: '',
-    placeholder: '',
-    caption: '',
-    required: false,
-    disabled: false,
-    aiConnected: true,
-    invalidText: '',
-    minLength: undefined,
-    maxLength: undefined,
-    rows: 1,
-    maxRowsVisible: 4,
-    textStrings: {
-      requiredText: 'Required',
-      errorText: 'Error',
-    },
-  },
-  render: (args) => {
-    return html`
-      <kyn-text-area
-        name=${args.name}
-        value=${args.value}
-        placeholder=${args.placeholder}
-        caption=${args.caption}
-        ?required=${args.required}
-        ?disabled=${args.disabled}
-        ?aiConnected=${args.aiConnected}
-        invalidText=${args.invalidText}
-        .textStrings=${args.textStrings}
-        minLength=${ifDefined(args.minLength)}
-        maxLength=${ifDefined(args.maxLength)}
-        rows=${args.rows}
-        maxRowsVisible=${args.maxRowsVisible}
-        @on-input=${(e) => action(e.type)(e)}
-        @keydown=${(e) => e.stopPropagation()}
-        label=${args.label}
-      >
-      </kyn-text-area>
-    `;
-  },
+const Template = (args) => {
+  return html`
+    <kyn-text-area
+      name=${args.name}
+      value=${args.value}
+      placeholder=${args.placeholder}
+      caption=${args.caption}
+      ?required=${args.required}
+      ?disabled=${args.disabled}
+      invalidText=${args.invalidText}
+      .textStrings=${args.textStrings}
+      minLength=${ifDefined(args.minLength)}
+      maxLength=${ifDefined(args.maxLength)}
+      rows=${args.rows}
+      ?aiConnected=${args.aiConnected}
+      maxRowsVisible=${args.maxRowsVisible}
+      @on-input=${(e) => action(e.type)(e)}
+      @keydown=${(e) => e.stopPropagation()}
+      label=${args.label}
+    >
+    </kyn-text-area>
+  `;
+};
+
+export const TextArea = Template.bind({});
+TextArea.args = {
+  ...args,
+};
+
+export const AIConnected = Template.bind({});
+AIConnected.args = {
+  ...args,
+  aiConnected: true,
+  rows: 1,
+  maxRowsVisible: 4,
 };
 
 TextArea.parameters = {
