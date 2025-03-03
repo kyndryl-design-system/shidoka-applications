cb062aade8cd22ae4583f84f672e60299eedacc0
Brian Patrick <brian.patrick3@kyndryl.com>
2025-03-03 08:53:16 -0600

    docs(inline-confirm): add story with card

--- src/components/reusable/inlineConfirm/inlineConfirm.stories.js
+++ src/components/reusable/inlineConfirm/inlineConfirm.stories.js
@@ -2,7 +2,8 @@ import { html } from 'lit-html';
 import { action } from '@storybook/addon-actions';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
 import './index';
-import '../button'; // Import the button component
+import '../button';
+import '../card';
 
 import deleteIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/24/delete.svg';
 
@@ -17,24 +18,51 @@ export default {
   ],
 };
 
-const Template = (args) => html`
-  <kyn-inline-confirm
-    .destructive=${args.destructive}
-    .anchorText=${args.anchorText}
-    .confirmText=${args.confirmText}
-    .cancelText=${args.cancelText}
-    .openRight=${args.openRight}
-    @on-confirm=${(e) => action('on-confirm')()}
-  >
-    ${unsafeSVG(deleteIcon)}
-  </kyn-inline-confirm>
-`;
-
-export const Default = Template.bind({});
-Default.args = {
+const args = {
   destructive: true,
   anchorText: 'Delete',
   confirmText: 'Confirm',
   cancelText: 'Cancel',
   openRight: false,
+};
+
+export const InlineConfirm = {
+  args,
+  render: (args) => {
+    return html`
+      <kyn-inline-confirm
+        .destructive=${args.destructive}
+        .anchorText=${args.anchorText}
+        .confirmText=${args.confirmText}
+        .cancelText=${args.cancelText}
+        .openRight=${args.openRight}
+        @on-confirm=${(e) => action('on-confirm')()}
+      >
+        ${unsafeSVG(deleteIcon)}
+      </kyn-inline-confirm>
+    `;
+  },
+};
+
+export const WithCard = {
+  args,
+  render: (args) => {
+    return html`
+      <kyn-card>
+        <div style="display: flex; align-items: center;">
+          Sample card content will go here lorem ipsum dolar.
+          <kyn-inline-confirm
+            .destructive=${args.destructive}
+            .anchorText=${args.anchorText}
+            .confirmText=${args.confirmText}
+            .cancelText=${args.cancelText}
+            .openRight=${args.openRight}
+            @on-confirm=${(e) => action('on-confirm')()}
+          >
+            ${unsafeSVG(deleteIcon)}
+          </kyn-inline-confirm>
+        </div>
+      </kyn-card>
+    `;
+  },
 };
