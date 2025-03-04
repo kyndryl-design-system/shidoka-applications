ca6ed490080820d545e0fa67b0051eaf9809555a
wbarbee <will.clayton.barbee@gmail.com>
2025-03-04 14:08:16 -0600

    feat: additional stories

--- src/stories/ai/aiAnswers.stories.js
+++ src/stories/ai/aiAnswers.stories.js
@@ -55,7 +55,75 @@ export const Default = {
           display: flex;
           gap: 20px;
           flex-wrap: wrap;
-          align-items: center;
+        }
+      </style>
+    `;
+  },
+};
+
+export const Centered = {
+  render: () => {
+    return html`
+      <div class="ai-answers-wrapper centered">
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 1"
+          description="AI Answer 1"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 1</kyn-button
+        >
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 2"
+          description="AI Answer 2"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 2</kyn-button
+        >
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 3"
+          description="AI Answer 3"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 3</kyn-button
+        >
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 4"
+          description="AI Answer 4"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 4</kyn-button
+        >
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 5"
+          description="AI Answer 5"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 5</kyn-button
+        >
+        <kyn-button
+          size="small"
+          kind="secondary-ai"
+          name="AI Answer 6"
+          description="AI Answer 6"
+          @on-click=${(e) => action(e.type)(e)}
+          >AI Answer 6</kyn-button
+        >
+      </div>
+
+      <style>
+        .ai-answers-wrapper {
+          display: flex;
+          gap: 20px;
+          flex-wrap: wrap;
+        }
+
+        .ai-answers-wrapper.centered {
+          justify-content: center;
         }
       </style>
     `;
--- src/stories/ai/aiPrompts.stories.js
+++ src/stories/ai/aiPrompts.stories.js
@@ -12,7 +12,7 @@ export default {
   },
 };
 
-export const WithAvatarInitials = {
+export const Default = {
   render: () => {
     return html`
       <div class="ai-prompts-wrapper">
@@ -62,6 +62,57 @@ export const WithAvatarInitials = {
             </div>
           </sample-card-component>
         </kyn-card>
+      </div>
+
+      <style>
+        .ai-prompts-wrapper {
+          display: flex;
+          gap: 20px;
+          flex-wrap: wrap;
+        }
+
+        .ai-prompts-wrapper kyn-card {
+          width: 250px;
+          font-size: 14px;
+        }
+
+        sample-card-component [slot='title'] {
+          font-weight: 500;
+          margin-top: 0;
+          margin-bottom: 8px;
+        }
+
+        sample-card-component [slot='description'] code {
+          font-size: 14px;
+        }
+
+        kyn-card::part(card-wrapper) {
+          height: 100%;
+        }
+      </style>
+    `;
+  },
+};
+
+export const Centered = {
+  render: () => {
+    return html`
+      <div class="ai-prompts-wrapper centered">
+        <kyn-card
+          type="clickable"
+          aiConnected
+          @on-card-click=${(e) => action(e.type)(e)}
+        >
+          <sample-card-component>
+            <div slot="title">Success Stories</div>
+            <div slot="description">
+              Help me find previous case studies or Success stories involving
+              <code>&lt;industry&gt;</code> or similar clients for
+              <code>&lt;business problem&gt;</code> or using
+              <code>&lt;Kyndryl service&gt;</code>
+            </div>
+          </sample-card-component>
+        </kyn-card>
 
         <kyn-card
           type="clickable"
@@ -69,11 +120,27 @@ export const WithAvatarInitials = {
           @on-card-click=${(e) => action(e.type)(e)}
         >
           <sample-card-component>
-            <div slot="title">Performance Analysis</div>
+            <div slot="title">Industry based Search</div>
             <div slot="description">
-              Analyze performance metrics for
-              <code>&lt;service area&gt;</code> and suggest improvements for
-              <code>&lt;specific metrics&gt;</code>
+              Search for documents tailored to the interests of
+              <code>&lt;customer&gt;</code> from
+              <code>&lt;industry&gt;</code> for a business opportunity focused
+              on <code>&lt;Kyndryl service&gt;</code>
+            </div>
+          </sample-card-component>
+        </kyn-card>
+
+        <kyn-card
+          type="clickable"
+          aiConnected
+          @on-card-click=${(e) => action(e.type)(e)}
+        >
+          <sample-card-component>
+            <div slot="title">Service fit</div>
+            <div slot="description">
+              Retrieve documents that highlight how our products/services can
+              address <code>&lt;customer specific needs&gt;</code> on customer
+              <code>&lt;business problem&gt;</code>
             </div>
           </sample-card-component>
         </kyn-card>
@@ -84,12 +151,14 @@ export const WithAvatarInitials = {
           display: flex;
           gap: 20px;
           flex-wrap: wrap;
-          align-items: stretch;
+        }
+
+        .ai-prompts-wrapper.centered {
+          justify-content: center;
         }
 
         .ai-prompts-wrapper kyn-card {
-          flex: 1;
-          min-width: 250px;
+          width: 250px;
           font-size: 14px;
         }
 
