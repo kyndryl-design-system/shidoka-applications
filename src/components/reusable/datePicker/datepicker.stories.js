e09f9ee5b6e3e114505846fe071e5fef40a7944d
wbarbee <will.clayton.barbee@gmail.com>
2025-03-05 16:58:13 -0600

    fix: better split between nested components in datepicker story

--- src/components/reusable/datePicker/datepicker.stories.js
+++ src/components/reusable/datePicker/datepicker.stories.js
@@ -256,82 +256,175 @@ DatepickerInModal.storyName = 'DatePicker In Modal';
 export const DatepickerInSideDrawer = {
   args: {
     ...DatePickerDefault.args,
-    locale: 'en',
-    name: 'date-picker-in-side-drawer',
-    dateFormat: 'Y-m-d',
-    staticPosition: false,
-    defaultDate: null,
-    caption: 'Datepicker in a modal.',
-    label: 'Date',
-    titleText: 'Select Date',
-    okText: 'OK',
-    cancelText: 'Cancel',
-    closeText: 'Close',
-    destructive: false,
-    okDisabled: false,
-    hideFooter: false,
-    hideCancelButton: false,
-    aiConnected: false,
-    disableScroll: false,
+    // Side Drawer args
     open: false,
     size: 'md',
+    titleText: 'Select Date',
     labelText: 'Label',
     submitBtnText: 'Ok',
     cancelBtnText: 'Cancel',
     submitBtnDisabled: false,
+    hideFooter: false,
+    destructive: false,
     showSecondaryButton: true,
     secondaryButtonText: 'Secondary',
+    hideCancelButton: false,
+    // Date Picker args
+    locale: 'en',
+    name: 'date-picker-in-side-drawer',
+    dateFormat: 'Y-m-d',
+    staticPosition: true,
+    defaultDate: null,
+    caption: 'Datepicker in a modal.',
+    label: 'Date',
+    defaultErrorMessage: '',
+    required: false,
+    value: '',
+    warnText: '',
+    invalidText: '',
+    disable: false,
+    enable: true,
+    mode: 'single',
+    errorAriaLabel: '',
+    errorTitle: '',
+    warningAriaLabel: '',
+    warningTitle: '',
+    datePickerDisabled: false,
+    twentyFourHourFormat: false,
+    minDate: '',
+    maxDate: '',
   },
   render: (args) => {
+    const {
+      open,
+      size,
+      titleText,
+      labelText,
+      submitBtnText,
+      cancelBtnText,
+      submitBtnDisabled,
+      hideFooter,
+      destructive,
+      showSecondaryButton,
+      secondaryButtonText,
+      hideCancelButton,
+      // DatePicker args
+      name,
+      locale,
+      label,
+      dateFormat,
+      defaultDate,
+      staticPosition,
+      defaultErrorMessage,
+      required,
+      value,
+      warnText,
+      invalidText,
+      disable,
+      enable,
+      mode,
+      caption,
+      errorAriaLabel,
+      errorTitle,
+      warningAriaLabel,
+      warningTitle,
+      datePickerDisabled,
+      twentyFourHourFormat,
+      minDate,
+      maxDate,
+    } = args;
+
+    const sideDrawerProps = {
+      open,
+      size,
+      titleText,
+      labelText,
+      submitBtnText,
+      cancelBtnText,
+      submitBtnDisabled,
+      hideFooter,
+      destructive,
+      showSecondaryButton,
+      secondaryButtonText,
+      hideCancelButton,
+    };
+
+    const datePickerProps = {
+      name,
+      locale,
+      label,
+      dateFormat,
+      defaultDate,
+      staticPosition,
+      defaultErrorMessage,
+      required,
+      size,
+      value,
+      warnText,
+      invalidText,
+      disable,
+      enable,
+      mode,
+      caption,
+      errorAriaLabel,
+      errorTitle,
+      warningAriaLabel,
+      warningTitle,
+      datePickerDisabled,
+      twentyFourHourFormat,
+      minDate,
+      maxDate,
+    };
+
     return html`
       <kyn-side-drawer
-        ?open=${args.open}
-        size=${args.size}
-        titleText=${args.titleText}
-        labelText=${args.labelText}
-        submitBtnText=${args.submitBtnText}
-        cancelBtnText=${args.cancelBtnText}
-        ?submitBtnDisabled=${args.submitBtnDisabled}
-        ?hideFooter=${args.hideFooter}
-        ?destructive=${args.destructive}
-        ?showSecondaryButton=${args.showSecondaryButton}
-        secondaryButtonText=${args.secondaryButtonText}
-        ?hideCancelButton=${args.hideCancelButton}
+        ?open=${sideDrawerProps.open}
+        size=${sideDrawerProps.size}
+        titleText=${sideDrawerProps.titleText}
+        labelText=${sideDrawerProps.labelText}
+        submitBtnText=${sideDrawerProps.submitBtnText}
+        cancelBtnText=${sideDrawerProps.cancelBtnText}
+        ?submitBtnDisabled=${sideDrawerProps.submitBtnDisabled}
+        ?hideFooter=${sideDrawerProps.hideFooter}
+        ?destructive=${sideDrawerProps.destructive}
+        ?showSecondaryButton=${sideDrawerProps.showSecondaryButton}
+        secondaryButtonText=${sideDrawerProps.secondaryButtonText}
+        ?hideCancelButton=${sideDrawerProps.hideCancelButton}
         @on-close=${(e) => action(e.type)(e)}
         @on-open=${(e) => action(e.type)(e)}
       >
         <kyn-button slot="anchor">Open Drawer</kyn-button>
 
         <div>
-          This drawer contains som example content and a DatePicker component.
+          This drawer contains some example content and a DatePicker component.
         </div>
         <br />
 
         <kyn-date-picker
-          .name=${args.name}
-          .locale=${args.locale}
-          .label=${args.label}
-          .dateFormat=${args.dateFormat}
-          .defaultDate=${args.defaultDate}
-          ?staticPosition=${args.staticPosition}
-          .defaultErrorMessage=${args.defaultErrorMessage}
-          ?required=${args.required}
-          .size=${args.size}
-          .value=${args.value}
-          .warnText=${args.warnText}
-          .invalidText=${args.invalidText}
-          .disable=${args.disable}
-          .enable=${args.enable}
-          .mode=${args.mode}
-          .caption=${args.caption}
-          .errorAriaLabel=${args.errorAriaLabel}
-          .errorTitle=${args.errorTitle}
-          .warningAriaLabel=${args.warningAriaLabel}
-          .warningTitle=${args.warningTitle}
-          ?datePickerDisabled=${args.datePickerDisabled}
-          ?twentyFourHourFormat=${args.twentyFourHourFormat}
-          .minDate=${args.minDate}
-          .maxDate=${args.maxDate}
+          .name=${datePickerProps.name}
+          .locale=${datePickerProps.locale}
+          .label=${datePickerProps.label}
+          .dateFormat=${datePickerProps.dateFormat}
+          .defaultDate=${datePickerProps.defaultDate}
+          ?staticPosition=${datePickerProps.staticPosition}
+          .defaultErrorMessage=${datePickerProps.defaultErrorMessage}
+          ?required=${datePickerProps.required}
+          .size=${datePickerProps.size}
+          .value=${datePickerProps.value}
+          .warnText=${datePickerProps.warnText}
+          .invalidText=${datePickerProps.invalidText}
+          .disable=${datePickerProps.disable}
+          .enable=${datePickerProps.enable}
+          .mode=${datePickerProps.mode}
+          .caption=${datePickerProps.caption}
+          .errorAriaLabel=${datePickerProps.errorAriaLabel}
+          .errorTitle=${datePickerProps.errorTitle}
+          .warningAriaLabel=${datePickerProps.warningAriaLabel}
+          .warningTitle=${datePickerProps.warningTitle}
+          ?datePickerDisabled=${datePickerProps.datePickerDisabled}
+          ?twentyFourHourFormat=${datePickerProps.twentyFourHourFormat}
+          .minDate=${datePickerProps.minDate}
+          .maxDate=${datePickerProps.maxDate}
           @on-change=${(e) => action(e.type)(e)}
           style="margin-left: 4px;"
         >
@@ -343,19 +436,7 @@ export const DatepickerInSideDrawer = {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin cursus,
           purus vitae egestas mollis, augue augue interdum quam, sit amet
           volutpat justo magna quis justo. Aliquam dapibus mi a arcu consequat,
-          sed placerat metus bibendum. Suspendisse pretium nibh Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Proin cursus, purus vitae
-          egestas mollis, augue augue interdum quam, sit amet volutpat justo
-          magna quis justo. Aliquam dapibus mi a arcu consequat, sed placerat
-          metus bibendum. Suspendisse pretium nibh Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Proin cursus, purus vitae egestas mollis,
-          augue augue interdum quam, sit amet volutpat justo magna quis justo.
-          Aliquam dapibus mi a arcu consequat, sed placerat metus bibendum.
-          Suspendisse pretium nibh Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Proin cursus, purus vitae egestas mollis, augue augue
-          interdum quam, sit amet volutpat justo magna quis justo. Aliquam
-          dapibus mi a arcu consequat, sed placerat metus bibendum. Suspendisse
-          pretium nibh
+          sed placerat metus bibendum. Suspendisse pretium nibh.
         </div>
       </kyn-side-drawer>
     `;
