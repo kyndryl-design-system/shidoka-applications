c76b6935d1593412d2fe98a93a8a75b543050f34
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-03-05 18:44:00 +0530

    chore: added story for testing

--- src/components/reusable/radioButton/radioButtonGroup.stories.js
+++ src/components/reusable/radioButton/radioButtonGroup.stories.js
@@ -52,3 +52,40 @@ export const RadioButtonGroup = {
     `;
   },
 };
+
+export const TestingButtonDisable = {
+  args: {
+    label: 'Label',
+    name: 'name',
+    value: '1',
+    required: false,
+    disabled: false,
+    horizontal: false,
+    invalidText: '',
+    description: 'Description',
+    textStrings: {
+      required: 'Required',
+      error: 'Error',
+    },
+  },
+  render: (args) => {
+    return html`
+      <kyn-radio-button-group
+        name=${args.name}
+        value=${args.value}
+        ?required=${args.required}
+        ?disabled=${args.disabled}
+        ?horizontal=${args.horizontal}
+        invalidText=${args.invalidText}
+        .textStrings=${args.textStrings}
+        label=${args.label}
+        @on-radio-group-change=${(e) => action(e.type)(e)}
+      >
+        <div slot="description">${args.description}</div>
+        <kyn-radio-button value="1"> Option 1 </kyn-radio-button>
+        <kyn-radio-button value="2"> Option 2 </kyn-radio-button>
+        <kyn-radio-button value="3" disabled> Option 3 </kyn-radio-button>
+      </kyn-radio-button-group>
+    `;
+  },
+};
