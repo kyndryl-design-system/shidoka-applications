4c277ef1779af0aa127ac8ec7c5012dde671f4ac
wbarbee <will.clayton.barbee@gmail.com>
2025-03-07 14:47:37 -0600

    fix: stories.js code to ensure that sameple-card-component elements do not show

--- src/stories/ai/aiPrompts.stories.js
+++ src/stories/ai/aiPrompts.stories.js
@@ -12,170 +12,177 @@ export default {
   },
 };
 
+const hideLogoAndSubtitleStyles = html`
+  <style>
+    sample-card-component .card-logo-container,
+    sample-card-component .card-subtitle {
+      display: none !important;
+    }
+  </style>
+`;
+
 export const Default = {
-  render: () => {
-    return html`
-      <div class="ai-prompts-wrapper">
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Success Stories</div>
-            <div slot="description">
-              Help me find previous case studies or Success stories involving
-              <code>&lt;industry&gt;</code> or similar clients for
-              <code>&lt;business problem&gt;</code> or using
-              <code>&lt;Kyndryl service&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
+  render: () => html`
+    ${hideLogoAndSubtitleStyles}
+    <div class="ai-prompts-wrapper">
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Success Stories</div>
+          <div slot="description">
+            Help me find previous case studies or Success stories involving
+            <code>&lt;industry&gt;</code> or similar clients for
+            <code>&lt;business problem&gt;</code> or using
+            <code>&lt;Kyndryl service&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
 
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Industry based Search</div>
-            <div slot="description">
-              Search for documents tailored to the interests of
-              <code>&lt;customer&gt;</code> from
-              <code>&lt;industry&gt;</code> for a business opportunity focused
-              on <code>&lt;Kyndryl service&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Industry based Search</div>
+          <div slot="description">
+            Search for documents tailored to the interests of
+            <code>&lt;customer&gt;</code> from <code>&lt;industry&gt;</code> for
+            a business opportunity focused on
+            <code>&lt;Kyndryl service&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
 
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Service fit</div>
-            <div slot="description">
-              Retrieve documents that highlight how our products/services can
-              address <code>&lt;customer specific needs&gt;</code> on customer
-              <code>&lt;business problem&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
-      </div>
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Service fit</div>
+          <div slot="description">
+            Retrieve documents that highlight how our products/services can
+            address <code>&lt;customer specific needs&gt;</code> on customer
+            <code>&lt;business problem&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
+    </div>
 
-      <style>
-        .ai-prompts-wrapper {
-          display: flex;
-          gap: 20px;
-          flex-wrap: wrap;
-        }
+    <style>
+      .ai-prompts-wrapper {
+        display: flex;
+        gap: 20px;
+        flex-wrap: wrap;
+      }
 
-        .ai-prompts-wrapper kyn-card {
-          width: 250px;
-          font-size: 14px;
-        }
+      .ai-prompts-wrapper kyn-card {
+        width: 250px;
+        font-size: 14px;
+      }
 
-        sample-card-component [slot='title'] {
-          font-weight: 500;
-          margin-top: 0;
-          margin-bottom: 8px;
-        }
+      sample-card-component [slot='title'] {
+        font-weight: 500;
+        margin-top: 0;
+        margin-bottom: 8px;
+      }
 
-        sample-card-component [slot='description'] code {
-          font-size: 14px;
-        }
+      sample-card-component [slot='description'] code {
+        font-size: 14px;
+      }
 
-        kyn-card::part(card-wrapper) {
-          height: 100%;
-        }
-      </style>
-    `;
-  },
+      kyn-card::part(card-wrapper) {
+        height: 100%;
+      }
+    </style>
+  `,
 };
 
 export const Centered = {
-  render: () => {
-    return html`
-      <div class="ai-prompts-wrapper centered">
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Success Stories</div>
-            <div slot="description">
-              Help me find previous case studies or Success stories involving
-              <code>&lt;industry&gt;</code> or similar clients for
-              <code>&lt;business problem&gt;</code> or using
-              <code>&lt;Kyndryl service&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
+  render: () => html`
+    ${hideLogoAndSubtitleStyles}
+    <div class="ai-prompts-wrapper centered">
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Success Stories</div>
+          <div slot="description">
+            Help me find previous case studies or Success stories involving
+            <code>&lt;industry&gt;</code> or similar clients for
+            <code>&lt;business problem&gt;</code> or using
+            <code>&lt;Kyndryl service&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
 
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Industry based Search</div>
-            <div slot="description">
-              Search for documents tailored to the interests of
-              <code>&lt;customer&gt;</code> from
-              <code>&lt;industry&gt;</code> for a business opportunity focused
-              on <code>&lt;Kyndryl service&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Industry based Search</div>
+          <div slot="description">
+            Search for documents tailored to the interests of
+            <code>&lt;customer&gt;</code> from <code>&lt;industry&gt;</code> for
+            a business opportunity focused on
+            <code>&lt;Kyndryl service&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
 
-        <kyn-card
-          type="clickable"
-          aiConnected
-          @on-card-click=${(e) => action(e.type)(e)}
-        >
-          <sample-card-component>
-            <div slot="title">Service fit</div>
-            <div slot="description">
-              Retrieve documents that highlight how our products/services can
-              address <code>&lt;customer specific needs&gt;</code> on customer
-              <code>&lt;business problem&gt;</code>
-            </div>
-          </sample-card-component>
-        </kyn-card>
-      </div>
+      <kyn-card
+        type="clickable"
+        aiConnected
+        @on-card-click=${(e) => action(e.type)(e)}
+      >
+        <sample-card-component>
+          <div slot="title">Service fit</div>
+          <div slot="description">
+            Retrieve documents that highlight how our products/services can
+            address <code>&lt;customer specific needs&gt;</code> on customer
+            <code>&lt;business problem&gt;</code>
+          </div>
+        </sample-card-component>
+      </kyn-card>
+    </div>
 
-      <style>
-        .ai-prompts-wrapper {
-          display: flex;
-          gap: 20px;
-          flex-wrap: wrap;
-        }
+    <style>
+      .ai-prompts-wrapper {
+        display: flex;
+        gap: 20px;
+        flex-wrap: wrap;
+      }
 
-        .ai-prompts-wrapper.centered {
-          justify-content: center;
-        }
+      .ai-prompts-wrapper.centered {
+        justify-content: center;
+      }
 
-        .ai-prompts-wrapper kyn-card {
-          width: 250px;
-          font-size: 14px;
-        }
+      .ai-prompts-wrapper kyn-card {
+        width: 250px;
+        font-size: 14px;
+      }
 
-        sample-card-component [slot='title'] {
-          font-weight: 500;
-          margin-top: 0;
-          margin-bottom: 8px;
-        }
+      sample-card-component [slot='title'] {
+        font-weight: 500;
+        margin-top: 0;
+        margin-bottom: 8px;
+      }
 
-        sample-card-component [slot='description'] code {
-          font-size: 14px;
-        }
+      sample-card-component [slot='description'] code {
+        font-size: 14px;
+      }
 
-        kyn-card::part(card-wrapper) {
-          height: 100%;
-        }
-      </style>
-    `;
-  },
+      kyn-card::part(card-wrapper) {
+        height: 100%;
+      }
+    </style>
+  `,
 };
