596fc633caf3a3159c43dc912027a4bf5a588271
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-04-08 16:10:04 +0530

    chore: added tooltip and editable input

--- src/components/reusable/sliderInput/sliderInput.stories.js
+++ src/components/reusable/sliderInput/sliderInput.stories.js
@@ -1,9 +1,6 @@
 import { html } from 'lit';
-import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
 import './index';
 import { action } from '@storybook/addon-actions';
-import '../tooltip';
-import infoIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/information.svg';
 
 export default {
   title: 'Components/Slider Input',
@@ -31,7 +28,8 @@ const args = {
   min: 0,
   max: 100,
   step: 1,
-  vertical: false,
+  editableInput: true,
+  multirange: false,
   invalidText: '',
 };
 
@@ -48,16 +46,60 @@ export const SliderInput = {
         invalidText=${args.invalidText}
         min=${args.min}
         max=${args.max}
-        step=${args.step}
-        ?vertical=${args.vertical}
+        ?editableInput=${args.editableInput}
         label=${args.label}
+        width=${args.width}
         @on-input=${(e) => action(e.type)(e)}
       >
-        <kyn-tooltip slot="tooltip">
-          <span slot="anchor" style="display:flex">${unsafeSVG(infoIcon)}</span>
-          tooltip
-        </kyn-tooltip>
       </kyn-slider-input>
+    `;
+  },
+};
+
+export const MultipleRangeControl = {
+  args: { ...args, step: 25, multirange: true },
+  render: (args) => {
+    return html`
+      <kyn-slider-input
+        name=${args.name}
+        value=${args.value}
+        caption=${args.caption}
+        ?hideLabel=${args.hideLabel}
+        ?disabled=${args.disabled}
+        invalidText=${args.invalidText}
+        ?editableInput=${args.editableInput}
+        min=${args.min}
+        max=${args.max}
+        step=${args.step}
+        label=${args.label}
+        multirange
+        @on-input=${(e) => action(e.type)(e)}
+      >
+        <div class="sliderticks" slot="tickmark">
+          <span>0</span>
+          <span>25</span>
+          <span>50</span>
+          <span>75</span>
+          <span>100</span>
+        </div>
+      </kyn-slider-input>
+      <style>
+        .sliderticks {
+          display: flex;
+          justify-content: space-between;
+          padding: 1px 0px 0px 8px;
+        }
+
+        .sliderticks span {
+          display: flex;
+          justify-content: center;
+          width: 1px;
+          height: 8px;
+          background: var(--kd-color-background-accent-tertiary);
+          line-height: 45px;
+          margin-bottom: 20px;
+        }
+      </style>
     `;
   },
 };
--- src/components/reusable/sliderInput/sliderInput.ts
+++ src/components/reusable/sliderInput/sliderInput.ts
@@ -3,6 +3,7 @@ import { customElement, property, query, state } from 'lit/decorators.js';
 import { deepmerge } from 'deepmerge-ts';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
+import { classMap } from 'lit/directives/class-map.js';
 import { FormMixin } from '../../../common/mixins/form-input';
 import silderInputScss from './sliderInput.scss';
 import errorIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/error-filled.svg';
@@ -19,6 +20,7 @@ const _defaultTextStrings = {
  * @prop {string} max - Maximum allowed range.
  * @prop {number} step - Number intervals.
  * @slot tooltip - Slot for tooltip.
+ * @slot tickmark - Slot for tickmarks.
  *
  */
 
@@ -46,17 +48,25 @@ export class SliderInput extends FormMixin(LitElement) {
   @property({ type: String })
   min!: string;
 
-  /** Specifies number intervals */
+  /** Specifies step intervals */
   @property({ type: Number })
-  step!: number;
+  step = 1;
 
   /** Visually hide the label. */
   @property({ type: Boolean })
   hideLabel = false;
 
   /** vertical orientation. */
+  // @property({ type: Boolean })
+  // vertical = false;
+
+  /** Set this to `true` for editable Input. */
   @property({ type: Boolean })
-  vertical = false;
+  editableInput = false;
+
+  /** Set this to `true` for multi range control. */
+  @property({ type: Boolean })
+  multirange = false;
 
   /** Customizable text strings. */
   @property({ type: Object })
@@ -68,6 +78,18 @@ export class SliderInput extends FormMixin(LitElement) {
   @state()
   _textStrings = _defaultTextStrings;
 
+  /** Internal tooltipVisible state.
+   * @internal
+   */
+  @state()
+  tooltipVisible = false;
+
+  /** Internal tooltipposition.
+   * @internal
+   */
+  @state()
+  tooltipPosition = 0;
+
   /**
    * Queries the <input> DOM element.
    * @ignore
@@ -76,6 +98,15 @@ export class SliderInput extends FormMixin(LitElement) {
   _inputEl!: HTMLInputElement;
 
   override render() {
+    const classes = {
+      'display-value': true,
+      'multi-range': this.multirange,
+    };
+
+    const orientationClasses = {
+      range: true,
+    };
+
     return html`
       <div class="text-input" ?disabled=${this.disabled}>
         <label
@@ -85,64 +116,62 @@ export class SliderInput extends FormMixin(LitElement) {
           <span>${this.label}</span>
           <slot name="tooltip"></slot>
         </label>
-        <div class="range ${this.vertical ? 'range-vertical' : ''}">
-          <input
-            type="range"
-            value="0"
-            id=${this.name}
-            name=${this.name}
-            min=${ifDefined(this.min)}
-            max=${ifDefined(this.max)}
-            step=${ifDefined(this.step)}
-            ?disabled=${this.disabled}
-            ?invalid=${this._isInvalid}
-            aria-invalid=${this._isInvalid}
-            aria-describedby=${this._isInvalid ? 'error' : ''}
-            class="range-input"
-            orient=${ifDefined(this.vertical ? 'vertical' : 'horizontal')}
-            @input=${(e: any) => this.handleInput(e)}
-            @keydown=${(e: KeyboardEvent) => this.handleKeydown(e)}
-          />
-          <div
-            class="display-value ${this.vertical
-              ? 'display-value-vertical'
-              : ''}"
-          >
-            ${this.value}
-          </div>
-        </div>
-        <!--  Tick data list to do -->
-        <!-- <div class="range">
-          <div class="range-slider">
-            <label for="range">Select a pleasant temperature:</label><br />
-            <input
-              type="range"
-              min="0"
-              max="100"
-              value="0"
-              class="range-input"
-              id="range4"
-              step="25"
-              @input=${(e: any) => this.handleInput(e)}
-            />
-            <div class="sliderticks"> 
-             This should be slotted
-              <option value="0" label="0"></option>
-              <option value="25" label="25"></option>
-              <option value="50" label="50"></option>
-              <option value="75" label="75"></option>
-              <option value="100" label="100"></option>
+        <div>
+          <div class="${classMap(orientationClasses)}">
+            <!-- empty div for tick mark alignment -->
+            <div class="${classMap(classes)}">${this.min}</div>
+            <div class="slider-wrapper">
+              <input
+                type="range"
+                value="0"
+                id=${this.name}
+                name=${this.name}
+                min=${ifDefined(this.min)}
+                max=${ifDefined(this.max)}
+                step=${ifDefined(this.step)}
+                ?disabled=${this.disabled}
+                ?invalid=${this._isInvalid}
+                aria-invalid=${this._isInvalid}
+                aria-describedby=${this._isInvalid ? 'error' : ''}
+                @input=${(e: any) => this.handleInput(e)}
+                @keydown=${(e: KeyboardEvent) => this.handleKeydown(e)}
+                @mouseover=${() => this.showTooltip()}
+                @mouseout=${() => this.hideTooltip()}
+                @keyup=${() => this.hideTooltip()}
+              />
+              <span
+                class="slider-tooltip"
+                style="left: ${this.getTooltipPosition()}px; visibility: ${this
+                  .tooltipVisible
+                  ? 'visible'
+                  : 'hidden'}"
+              >
+                ${this.value}
+              </span>
+              <!-- slot for tickmark varient  -->
+              ${this.multirange ? html`<slot name="tickmark"></slot>` : null}
+            </div>
+            <div class="${classMap(classes)}">${this.max}</div>
+            <div class="${classMap(classes)}">
+              ${this.editableInput
+                ? html`<input
+                    type="number"
+                    .value=${this.value}
+                    min=${ifDefined(this.min)}
+                    max=${ifDefined(this.max)}
+                    step=${ifDefined(this.step)}
+                    ?invalid=${this._isInvalid}
+                    aria-invalid=${this._isInvalid}
+                    aria-describedby=${this._isInvalid ? 'error' : ''}
+                    @input=${this.handleTextInput}
+                    ?disabled=${this.disabled}
+                    aria-label="editable range input"
+                  />`
+                : null}
             </div>
           </div>
+        </div>
 
-          <div
-            class="display-value ${this.vertical
-          ? 'display-value-vertical'
-          : ''}"
-          >
-            ${this.value}
-          </div>
-        </div> -->
         ${this.caption !== ''
           ? html` <div class="caption">${this.caption}</div> `
           : null}
@@ -163,6 +192,68 @@ export class SliderInput extends FormMixin(LitElement) {
     `;
   }
 
+  // Calculate the current position based on the slider's value
+  private handleTooltipPosition(inputWidth: number) {
+    const value = parseFloat(this.value);
+    const min = parseFloat(this.min);
+    const max = parseFloat(this.max);
+
+    // Calculate the position of the slider value
+    const pos = (value - min) / (max - min);
+    // thumb width 20px
+    const thumbCorrect = 20 * (pos - 0.5) * -1;
+    if (inputWidth) {
+      this.tooltipPosition = Math.floor(
+        pos * inputWidth - 25 / 4 + thumbCorrect
+      );
+    }
+    console.log('titlepos----', this.tooltipPosition);
+    this.requestUpdate();
+  }
+  // Handle show the tooltip
+  private showTooltip() {
+    this.tooltipVisible = true;
+  }
+
+  // Handle hide the tooltip
+  private hideTooltip() {
+    this.tooltipVisible = false;
+  }
+
+  // Get tooltip position
+  private getTooltipPosition() {
+    return this.tooltipPosition;
+  }
+
+  // Handle text input
+  private handleTextInput(e: Event) {
+    const input = e.target as HTMLInputElement;
+    const newValue = input.value;
+
+    // Ensure the value is within bounds (min and max)
+    if (newValue !== '') {
+      const numValue = parseFloat(newValue);
+      if (
+        !isNaN(numValue) &&
+        numValue >= parseFloat(this.min) &&
+        numValue <= parseFloat(this.max)
+      ) {
+        this.value = numValue;
+        this._validate(true, false);
+        this.emitDispatchEvent(e);
+      } else {
+        this._internalValidationMsg = `Value must be between ${this.min} and ${this.max}`;
+        input.setCustomValidity(
+          `Value must be between ${this.min} and ${this.max}`
+        );
+        this._isInvalid = true;
+      }
+    }
+    this.emitDispatchEvent(e);
+  }
+
+  // Handle input event
+  // This is triggered when the user interacts with the slider
   private handleInput(e: any) {
     if (this._isInvalid) return;
     this.value = e.target.value;
@@ -206,6 +297,7 @@ export class SliderInput extends FormMixin(LitElement) {
     }
   }
 
+  // Handle the track slider fill
   private fillTrackSlider() {
     const inputEl = this.shadowRoot?.querySelector('input');
     if (inputEl) {
@@ -221,30 +313,94 @@ export class SliderInput extends FormMixin(LitElement) {
     }
   }
 
+  // Handle keydown event
+  // This is triggered when the user presses a key while the slider is focused
   private handleKeydown(e: KeyboardEvent) {
     if (this.disabled || e.key === ' ' || e.key === 'Enter') {
       e.preventDefault();
       return;
     }
-    let newValue = parseFloat(this.value);
     const step = this.step;
     const min = parseFloat(this.min);
     const max = parseFloat(this.max);
+    if (this.multirange) {
+      this.handleMultiRangeKeydown(e, step, min, max);
+    } else {
+      this.handleSingleRangeKeydown(e, step, min, max);
+    }
+    this.emitDispatchEvent(e);
+  }
 
-    switch (e.key) {
+  private handleMultiRangeKeydown(
+    e: KeyboardEvent,
+    step: number,
+    min: number,
+    max: number
+  ) {
+    const currentValue = this.value
+      .split(',')
+      .map((v: string) => parseFloat(v.trim()));
+    const stepIndex = this._getHandleIndex();
+    if (stepIndex === -1) return;
+    const newValue = this.calculateNewValue(
+      currentValue[stepIndex],
+      e.key,
+      step,
+      min,
+      max
+    );
+    currentValue[stepIndex] = newValue;
+    this.value = currentValue.join(',');
+  }
+
+  private handleSingleRangeKeydown(
+    e: KeyboardEvent,
+    step: number,
+    min: number,
+    max: number
+  ) {
+    const currentValue = parseFloat(this.value);
+    if (isNaN(currentValue)) {
+      console.warn('Invalid current value:', this.value);
+      return;
+    }
+    const newValue = this.calculateNewValue(
+      parseFloat(this.value),
+      e.key,
+      step,
+      min,
+      max
+    );
+    this.value = newValue.toString();
+  }
+
+  private calculateNewValue(
+    currentValue: number,
+    key: string,
+    step: number,
+    min: number,
+    max: number
+  ): number {
+    switch (key) {
       case 'ArrowUp':
       case 'ArrowRight':
-        // Increase value
-        newValue = Math.min(newValue + step, max);
-        break;
+        return Math.min(currentValue + step, max);
       case 'ArrowDown':
       case 'ArrowLeft':
-        // Decrease value
-        newValue = Math.max(newValue - step, min);
-        break;
+        return Math.max(currentValue - step, min);
+      default:
+        return currentValue;
     }
-    this.value = newValue.toString();
-    this.emitDispatchEvent(e);
+  }
+
+  private _getHandleIndex(): number {
+    const inputEl = this.shadowRoot?.querySelector('input');
+    if (!inputEl) return -1;
+    const handles = Array.from(inputEl.querySelectorAll('.handle')); // Assuming each handle has the class `.handle`
+    const focusedHandle = handles.findIndex(
+      (handle) => handle === document.activeElement
+    );
+    return focusedHandle;
   }
 
   override updated(changedProps: any) {
@@ -256,8 +412,8 @@ export class SliderInput extends FormMixin(LitElement) {
       // Handle changes to `value`
       if (inputEl) {
         this.value = inputEl?.value;
-        console.log('value---', this.value);
         this.fillTrackSlider();
+        this.handleTooltipPosition(inputEl.offsetWidth);
       }
     }
     // Handle changes to `min` or `max`
@@ -275,8 +431,8 @@ export class SliderInput extends FormMixin(LitElement) {
         this.value = currentValue.toString();
         inputEl.min = this.min;
         inputEl.max = this.max;
-        console.log('min max---', this.value);
         this.fillTrackSlider();
+        this.handleTooltipPosition(inputEl.offsetWidth);
       }
     }
   }
