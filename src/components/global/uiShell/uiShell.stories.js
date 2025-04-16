10849a6c54f42ec4dd349af4beb4b3c7fd6b78c1
wbarbee <will.clayton.barbee@gmail.com>
2025-04-16 08:49:11 -0500

    chore: leave ui shell unlinked

--- src/components/global/uiShell/uiShell.stories.js
+++ src/components/global/uiShell/uiShell.stories.js
@@ -17,7 +17,7 @@ export default {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/9Q2XfTSxfzTXfNe2Bi8KDS/Component-Viewer?node-id=1-371547&p=f&m=dev',
+      url: '',
     },
   },
   decorators: [
