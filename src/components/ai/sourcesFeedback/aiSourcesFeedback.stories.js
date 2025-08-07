41fabdd3e986e1fbf028b80cdb8cafea9cd8d64d
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-08-07 14:40:47 +0530

    chore: updated figma link

--- src/components/ai/sourcesFeedback/aiSourcesFeedback.stories.js
+++ src/components/ai/sourcesFeedback/aiSourcesFeedback.stories.js
@@ -16,7 +16,7 @@ export default {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/9Q2XfTSxfzTXfNe2Bi8KDS/Component-Viewer?node-id=7-300053&p=f&m=dev',
+      url: 'https://www.figma.com/design/STWRRCgjIeXAZlIrIOJeTF/Ai-Component-Library?node-id=26610-251300&p=f&m=dev',
     },
   },
 };
