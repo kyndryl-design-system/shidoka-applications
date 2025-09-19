f82a552196ac8875027083823465baf28b079cc4
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-09-19 20:17:40 +0530

    chore: added more fix

--- src/components/reusable/metaData/metaData.stories.js
+++ src/components/reusable/metaData/metaData.stories.js
@@ -1,6 +1,6 @@
 import { html } from 'lit';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
-import smCube from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/cube.svg';
+import smCube from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/cube.svg';
 import './index';
 
 export default {
@@ -9,64 +9,174 @@ export default {
   parameters: {
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/qyPEUQckxj8LUgesi1OEES/Component-Library?node-id=39504-5898&m=dev',
+      url: 'https://www.figma.com/design/qyPEUQckxj8LUgesi1OEES/Component-Library?node-id=39511-307374&m=dev',
     },
   },
 };
 
 const args = {
-  labelText:
-    'Label/Title Label/Title Label/Title Label/Title Label/Title Label/Title Label/Title Label/Title',
-  vertical: true,
+  labelText: '',
+  horizontal: false,
   showIcon: true,
   showLabel: true,
   showValue: true,
 };
 
 export const MetaData = {
-  args,
+  args: {
+    ...args,
+    labelText:
+      'I am very Long title text that should be truncated if it exceeds the container width',
+  },
   render: (args) => {
     return html`
-      <!-- <div style="max-width: 500px;"> -->
+      <div style="max-width: 500px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?showIcon=${args.showIcon}
+          ?showLabel=${args.showLabel}
+          ?showValue=${args.showValue}
+        >
+          <div class="example">
+            <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
+              ${unsafeSVG(smCube)}
+            </div>
+            <span class="kd-type--ui-02 kd-type--weight-medium">Slot</span>
+          </div>
+        </kyn-meta-data>
+      </div>
+      <style>
+        .example {
+          flex-grow: 1;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          justify-content: center;
+          height: 31px;
+          border-radius: 4px;
+          padding: 4px;
+          background: var(--kd-color-background-container-subtle);
+          border: 1px dashed var(--kd-color-utility-variant-border);
+          .cube-icon {
+            display: flex;
+            color: var(--kd-color-icon-brand);
+
+            svg {
+              height: 23px;
+              width: 23px;
+            }
+          }
+        }
+      </style>
+    `;
+  },
+};
+
+export const OtherContent = {
+  args: { ...args, labelText: 'Label' },
+  render: (args) => {
+    return html`
+      <div style="max-width: 500px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?showIcon=${args.showIcon}
+          ?showLabel=${args.showLabel}
+          ?showValue=${args.showValue}
+        >
+          <div
+            style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
+            class="kd-type--body-02 example-content"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum
+          </div>
+        </kyn-meta-data>
+      </div>
+      <style>
+        .example-content {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 14px;
+          border-radius: 4px;
+        }
+      </style>
+    `;
+  },
+};
+
+export const ScrollableContent = {
+  args: { ...args, labelText: 'Label' },
+  render: (args) => {
+    return html`
+      <div style="max-width: 500px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?showIcon=${args.showIcon}
+          ?showLabel=${args.showLabel}
+          ?showValue=${args.showValue}
+          scrollableContent
+          style="height: 150px;"
+        >
+          <div
+            style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
+            class="kd-type--body-02 example-content"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const truncatedTextContent = {
+  args: { ...args, labelText: 'Label' },
+  render: (args) => {
+    return html`
       <kyn-meta-data
-        ?vertical=${args.vertical}
+        ?horizontal=${args.horizontal}
         labelText=${args.labelText}
         ?showIcon=${args.showIcon}
         ?showLabel=${args.showLabel}
         ?showValue=${args.showValue}
+        truncateContent
       >
-        <div class="example">
-          <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
-            ${unsafeSVG(smCube)}<span
-              style="    color: var(--kd-color-text-level-primary);"
-              class="kd-type--ui-01 kd-type--weight-medium"
-            >
-              Slot Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-              voluptate velit esse cillum</span
-            >
-          </div>
+        <div
+          style="color: var(--kd-color-text-level-secondary);line-height: 18px;overflow: hidden; text-overflow: ellipsis;"
+          class="kd-type--body-02"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
         </div>
       </kyn-meta-data>
-      <!-- </div> -->
-      <style>
-        .example {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background: var(--kd-color-background-container-subtle);
-          border: 1px dashed var(--kd-color-utility-variant-border);
-          border-radius: 4px;
-          width: 100%;
-        }
-      </style>
     `;
   },
 };
--- src/components/reusable/metaData/metaData.ts
+++ src/components/reusable/metaData/metaData.ts
@@ -2,6 +2,7 @@ import { LitElement, html, unsafeCSS } from 'lit';
 import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import { customElement, property } from 'lit/decorators.js';
 import testingIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/testing.svg';
+import { classMap } from 'lit/directives/class-map.js';
 import Styles from './metaData.scss?inline';
 
 /**
@@ -35,30 +36,62 @@ export class MetaData extends LitElement {
   @property({ type: Boolean })
   accessor showValue = true;
 
-  /** Vertical orientation. Default is `true` */
+  /** Horizontal orientation. Default is `false` */
   @property({ type: Boolean })
-  accessor vertical = false;
+  accessor horizontal = false;
+
+  /** Adds scrollable overflow to the slot content. Default is `false` */
+  @property({ type: Boolean })
+  accessor scrollableContent = false;
+
+  /** Truncate long content. Default is `false`. Note: Useful for text content */
+  @property({ type: Boolean })
+  accessor truncateContent = false;
 
   override render() {
+    const metaIcon = {
+      'meta-icon': true,
+      'v-align': !this.horizontal,
+    };
+    const metaWrapper = {
+      'meta-wrapper': true,
+      'horizontal-align': this.horizontal,
+    };
+    const metaLabel = {
+      'meta-label': true,
+      'h-align': this.horizontal,
+    };
+    const metaValue = {
+      'meta-value': true,
+      scrollable: this.scrollableContent,
+      truncate: this.truncateContent,
+    };
     return html`
       <div class="meta-container">
         ${this.showIcon
           ? html`<slot name="icon">
-              <span class="meta-icon ${this.vertical ? 'v-align' : ''}">
+              <span class="${classMap(metaIcon)}">
                 ${unsafeSVG(testingIcon)}
               </span>
             </slot>`
-          : ''}
-        ${this.showIcon ? html` <div class="spacer"></div> ` : ''}
-        <div class="meta-wrapper ${this.vertical ? 'vertical' : ''}">
+          : null}
+        ${this.showIcon ? html` <div class="spacer"></div> ` : null}
+        <div class="${classMap(metaWrapper)}">
           ${this.showLabel
             ? html`
-                <div class="meta-label ${!this.vertical ? 'h-align' : ''}">
-                  ${this.labelText}
+                <div class="${classMap(metaLabel)}">${this.labelText}</div>
+              `
+            : null}
+          ${this.showValue
+            ? html`
+                <div
+                  class="${classMap(metaValue)}"
+                  tabindex=${this.scrollableContent ? '0' : '-1'}
+                >
+                  <slot></slot>
                 </div>
               `
-            : ''}
-          ${this.showValue ? html` <slot></slot> ` : ''}
+            : null}
         </div>
       </div>
     `;
