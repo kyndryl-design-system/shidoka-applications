2446bd4564713b08d2e9161cd25e2baf8ea47d85
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-09-22 17:50:40 +0530

    chore: review fix

--- src/components/reusable/metaData/metaData.stories.js
+++ src/components/reusable/metaData/metaData.stories.js
@@ -1,7 +1,10 @@
 import { html } from 'lit';
 import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
 import smCube from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/cube.svg';
+import testingIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/testing.svg';
 import './index';
+import { LinkWithIcon as Link } from '../link/Link.stories.js';
+import { Badge } from '../badge/badge.stories.js';
 
 export default {
   title: 'Components/Meta Data',
@@ -12,40 +15,8 @@ export default {
       url: 'https://www.figma.com/design/qyPEUQckxj8LUgesi1OEES/Component-Library?node-id=39511-307374&m=dev',
     },
   },
-};
-
-const args = {
-  labelText: '',
-  horizontal: false,
-  showIcon: true,
-  showLabel: true,
-  showValue: true,
-};
-
-export const MetaData = {
-  args: {
-    ...args,
-    labelText:
-      'I am very Long title text that should be truncated if it exceeds the container width',
-  },
-  render: (args) => {
-    return html`
-      <div style="max-width: 500px;">
-        <kyn-meta-data
-          ?horizontal=${args.horizontal}
-          labelText=${args.labelText}
-          ?showIcon=${args.showIcon}
-          ?showLabel=${args.showLabel}
-          ?showValue=${args.showValue}
-        >
-          <div class="example">
-            <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
-              ${unsafeSVG(smCube)}
-            </div>
-            <span class="kd-type--ui-02 kd-type--weight-medium">Slot</span>
-          </div>
-        </kyn-meta-data>
-      </div>
+  decorators: [
+    (story) => html`
       <style>
         .example {
           flex-grow: 1;
@@ -68,41 +39,6 @@ export const MetaData = {
             }
           }
         }
-      </style>
-    `;
-  },
-};
-
-export const OtherContent = {
-  args: { ...args, labelText: 'Label' },
-  render: (args) => {
-    return html`
-      <div style="max-width: 500px;">
-        <kyn-meta-data
-          ?horizontal=${args.horizontal}
-          labelText=${args.labelText}
-          ?showIcon=${args.showIcon}
-          ?showLabel=${args.showLabel}
-          ?showValue=${args.showValue}
-        >
-          <div
-            style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
-            class="kd-type--body-02 example-content"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum
-          </div>
-        </kyn-meta-data>
-      </div>
-      <style>
         .example-content {
           display: flex;
           align-items: center;
@@ -110,7 +46,183 @@ export const OtherContent = {
           font-size: 14px;
           border-radius: 4px;
         }
+        .line_height {
+          line-height: 18px;
+        }
+        table {
+          width: 100%;
+          border-collapse: collapse;
+        }
+
+        td {
+          padding: 16px;
+          text-align: left;
+        }
+
+        tr:not(:first-child) td {
+          border-top: 1px solid var(--kd-color-border-level-tertiary);
+        }
       </style>
+      ${story()}
+    `,
+  ],
+};
+
+const args = {
+  labelText: 'Label',
+  horizontal: false,
+  noBackground: false,
+};
+
+export const MetaData = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="icon">${unsafeSVG(testingIcon)}</div>
+          <div class="example">
+            <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
+              ${unsafeSVG(smCube)}
+            </div>
+            <span class="kd-type--ui-02 kd-type--weight-medium">Slot</span>
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithText = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?noBackground=${args.noBackground}
+        >
+          <div
+            style="color: var(--kd-color-text-level-secondary);"
+            class="example-content line_height"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithLink = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?noBackground=${args.noBackground}
+        >
+          <div class="line_height">
+            ${Link.render({ standalone: true, ...Link.args })}
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithDate = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?noBackground=${args.noBackground}
+        >
+          <div class="line_height">09/18/2025</div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithBadge = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          labelText=${args.labelText}
+          ?noBackground=${args.noBackground}
+        >
+          <div style="line-height: 18px;">
+            ${Badge.render({
+              ...Badge.args,
+              status: 'success',
+              label: 'Badge Label',
+            })}
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithTable = {
+  args,
+  render: (args) => {
+    return html`
+      <h4>With Table</h4>
+      <br />
+      <kyn-meta-data
+        ?horizontal=${args.horizontal}
+        labelText=${args.labelText}
+        ?noBackground=${args.noBackground}
+      >
+        <div slot="icon">${unsafeSVG(testingIcon)}</div>
+        <table style="background: var(--kd-color-background-table-row);">
+          <tr>
+            <td>First Name</td>
+            <td>Arya</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>Stark</td>
+          </tr>
+          <tr>
+            <td>Birthday</td>
+            <td>February 15</td>
+          </tr>
+          <tr>
+            <td>Age</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>Full Name</td>
+            <td>Arya Stark</td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>Female</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>arya.stark@winterfell.com</td>
+          </tr>
+        </table>
+      </kyn-meta-data>
     `;
   },
 };
@@ -124,14 +236,12 @@ export const ScrollableContent = {
           ?horizontal=${args.horizontal}
           labelText=${args.labelText}
           ?showIcon=${args.showIcon}
-          ?showLabel=${args.showLabel}
-          ?showValue=${args.showValue}
           scrollableContent
           style="height: 150px;"
         >
           <div
             style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
-            class="kd-type--body-02 example-content"
+            class="example-content"
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -146,37 +256,6 @@ export const ScrollableContent = {
           </div>
         </kyn-meta-data>
       </div>
-    `;
-  },
-};
-
-export const truncatedTextContent = {
-  args: { ...args, labelText: 'Label' },
-  render: (args) => {
-    return html`
-      <kyn-meta-data
-        ?horizontal=${args.horizontal}
-        labelText=${args.labelText}
-        ?showIcon=${args.showIcon}
-        ?showLabel=${args.showLabel}
-        ?showValue=${args.showValue}
-        truncateContent
-      >
-        <div
-          style="color: var(--kd-color-text-level-secondary);line-height: 18px;overflow: hidden; text-overflow: ellipsis;"
-          class="kd-type--body-02"
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        </div>
-      </kyn-meta-data>
     `;
   },
 };
--- src/components/reusable/metaData/metaData.ts
+++ src/components/reusable/metaData/metaData.ts
@@ -1,14 +1,11 @@
 import { LitElement, html, unsafeCSS } from 'lit';
-import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
-import { customElement, property } from 'lit/decorators.js';
-import testingIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/testing.svg';
+import { customElement, property, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import Styles from './metaData.scss?inline';
-
 /**
  * MetaData component.
- * @slot icon - Slot for custom icon.
  * @slot unnamed - Slot for other content.
+ * @slot icon - Slot for icon.
  */
 @customElement('kyn-meta-data')
 export class MetaData extends LitElement {
@@ -18,37 +15,29 @@ export class MetaData extends LitElement {
   @property({ type: String })
   accessor labelText = '';
 
-  /**
-   * Show icon. Default is `true`.
-   */
-  @property({ type: Boolean })
-  accessor showIcon = true;
-
-  /**
-   * Show label. Default is `true`.
-   */
-  @property({ type: Boolean })
-  accessor showLabel = true;
-
-  /**
-   * Show value content. Default is `true`.
-   */
-  @property({ type: Boolean })
-  accessor showValue = true;
-
   /** Horizontal orientation. Default is `false` */
   @property({ type: Boolean })
   accessor horizontal = false;
+
+  /** No background. Default is `false` */
+  @property({ type: Boolean })
+  accessor noBackground = false;
 
   /** Adds scrollable overflow to the slot content. Default is `false` */
   @property({ type: Boolean })
   accessor scrollableContent = false;
 
-  /** Truncate long content. Default is `false`. Note: Useful for text content */
-  @property({ type: Boolean })
-  accessor truncateContent = false;
+  /** Determine the icon slot has content.
+   * @internal
+   */
+  @state()
+  private accessor hasIcon = false;
 
   override render() {
+    const metaContainer = {
+      'meta-container': true,
+      'no-background': this.noBackground,
+    };
     const metaIcon = {
       'meta-icon': true,
       'vertical-icon-align': !this.horizontal,
@@ -59,42 +48,51 @@ export class MetaData extends LitElement {
     };
     const metaLabel = {
       'meta-label': true,
-      'horizontal-label-align': this.horizontal,
     };
     const metaValue = {
       'meta-value': true,
       scrollable: this.scrollableContent,
-      truncate: this.truncateContent,
     };
     return html`
-      <div class="meta-container">
-        ${this.showIcon
-          ? html`<slot name="icon">
-              <span class="${classMap(metaIcon)}">
-                ${unsafeSVG(testingIcon)}
-              </span>
-            </slot>`
-          : null}
-        ${this.showIcon ? html` <div class="spacer"></div> ` : null}
+      <div class="${classMap(metaContainer)}">
+        <div
+          class="${classMap(metaIcon)}"
+          style=${!this.hasIcon ? 'display: none;' : ''}
+        >
+          <slot name="icon" @slotchange=${() => this.onIconSlotChange()}></slot>
+        </div>
+        ${this.hasIcon ? html` <div class="spacer"></div> ` : null}
         <div class="${classMap(metaWrapper)}">
-          ${this.showLabel
+          ${this.labelText !== '' || this.iconSlot?.assignedElements().length
             ? html`
                 <div class="${classMap(metaLabel)}">${this.labelText}</div>
               `
             : null}
-          ${this.showValue
-            ? html`
-                <div
-                  class="${classMap(metaValue)}"
-                  tabindex=${this.scrollableContent ? '0' : '-1'}
-                >
-                  <slot></slot>
-                </div>
-              `
-            : null}
+          <div
+            class="${classMap(metaValue)}"
+            tabindex=${this.scrollableContent ? '0' : '-1'}
+          >
+            <slot></slot>
+          </div>
         </div>
       </div>
     `;
+  }
+
+  override firstUpdated() {
+    this.hasIcon =
+      this.iconSlot?.assignedElements({ flatten: true }).length > 0;
+  }
+
+  private onIconSlotChange() {
+    this.hasIcon =
+      this.iconSlot?.assignedElements({ flatten: true }).length > 0;
+  }
+
+  private get iconSlot(): HTMLSlotElement {
+    return this.shadowRoot!.querySelector(
+      'slot[name="icon"]'
+    )! as HTMLSlotElement;
   }
 }
 
