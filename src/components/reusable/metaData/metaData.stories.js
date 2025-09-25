edc5d17efa852dcaf92827ee7e79cd185cd8a869
kumarivijeta <60390189+kumarivijeta@users.noreply.github.com>
2025-09-25 20:33:24 +0530

    feat(MetaComponent): added new meta component (#597)
    * feat(MetaComponent): added new meta component
    
    * chore: added more fix
    
    * chore: rename var
    
    * chore: review fix
    
    * chore: added label slot
    
    * chore: marco review fix
    
    * chore: review fixes
    
    * chore: review fix
    
    * chore: minor fix
    
    * chore: fixes
    
    * chore: table responsive fix
    
    * chore: run ele
    
    * chore: review fixes
    
    * chore: reverted
    
    * chore: minor fix
    
    * chore: added style to center label

---
+++ src/components/reusable/metaData/metaData.stories.js
@@ -0,0 +1,509 @@
+import { html } from 'lit';
+import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
+import smCube from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/cube.svg';
+import testingIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/testing.svg';
+import './index';
+import { LinkWithIcon as Link } from '../link/Link.stories.js';
+import { Badge } from '../badge/badge.stories.js';
+import '../table';
+import { repeat } from 'lit/directives/repeat.js';
+import { characters } from '../table/story-helpers/ultils.sample';
+
+export default {
+  title: 'Components/Meta Data',
+  component: 'kyn-meta-data',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/qyPEUQckxj8LUgesi1OEES/Component-Library?node-id=39511-307374&m=dev',
+    },
+  },
+  decorators: [
+    (story) => html`
+      <style>
+        .example {
+          flex-grow: 1;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          justify-content: center;
+          height: 31px;
+          border-radius: 4px;
+          padding: 4px;
+          background: var(--kd-color-background-container-subtle);
+          border: 1px dashed var(--kd-color-utility-variant-border);
+          .cube-icon {
+            display: flex;
+            align-items: center;
+            color: var(--kd-color-icon-brand);
+
+            svg {
+              height: 23px;
+              width: 23px;
+            }
+          }
+        }
+        .example-content {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 14px;
+          border-radius: 4px;
+        }
+        .line_height {
+          line-height: 18px;
+        }
+        .meta-content {
+          width: 100%;
+          overflow-x: auto;
+        }
+        .label-center {
+          display: inline-flex;
+          align-items: center;
+        }
+      </style>
+      ${story()}
+    `,
+  ],
+};
+
+const args = {
+  horizontal: false,
+  noBackground: false,
+};
+
+export const Default = {
+  args: { ...args },
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label" class="label-center">Label</div>
+          <div class="example">
+            <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
+              ${unsafeSVG(smCube)}
+            </div>
+            <span class="kd-type--ui-02 kd-type--weight-medium">Slot</span>
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithIcon = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label">Label</div>
+          <div slot="icon">${unsafeSVG(testingIcon)}</div>
+          <div class="example">
+            <div class="cube-icon" style="color:var(--kd-color-icon-brand);">
+              ${unsafeSVG(smCube)}
+            </div>
+            <span class="kd-type--ui-02 kd-type--weight-medium">Slot</span>
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithText = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label">Label</div>
+          <div
+            style="color: var(--kd-color-text-level-secondary);"
+            class="example-content line_height"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithLink = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label">Label</div>
+          <div class="line_height">
+            ${Link.render({ standalone: true, ...Link.args })}
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithDate = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label">Label</div>
+          <div class="line_height">09/18/2025</div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithBadge = {
+  args,
+  render: (args) => {
+    return html`
+      <div style="max-width: 300px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+        >
+          <div slot="label">Label</div>
+          <div style="line-height: 18px;">
+            ${Badge.render({
+              ...Badge.args,
+              status: 'success',
+              label: 'Badge Label',
+            })}
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const WithTable = {
+  args: {
+    ...args,
+  },
+  parameters: {
+    a11y: {
+      disable: true,
+    },
+  },
+  render: (args) => {
+    return html`
+      <kyn-meta-data
+        ?horizontal=${args.horizontal}
+        ?noBackground=${args.noBackground}
+      >
+        <div slot="label">Label</div>
+        <div slot="icon">${unsafeSVG(testingIcon)}</div>
+        <div class="meta-content">
+          <kyn-table>
+            <kyn-thead>
+              <kyn-header-tr>
+                <kyn-th .align=${'center'}>ID</kyn-th>
+                <kyn-th>First Name</kyn-th>
+                <kyn-th>Last Name</kyn-th>
+                <kyn-th>Birthday</kyn-th>
+                <kyn-th .align=${'right'}>Age</kyn-th>
+                <kyn-th>Full Name</kyn-th>
+              </kyn-header-tr>
+            </kyn-thead>
+            <kyn-tbody>
+              ${repeat(
+                characters,
+                (row) => row.id,
+                (row) => html`
+                  <kyn-tr .rowId=${String(row.id)} key="row-${row.id}">
+                    <kyn-td .align=${'center'}>${row.id}</kyn-td>
+                    <kyn-td>${row.firstName}</kyn-td>
+                    <kyn-td>${row.lastName}</kyn-td>
+                    <kyn-td>${row.birthday}</kyn-td>
+                    <kyn-td .align=${'right'}>${row.age}</kyn-td>
+                    <kyn-td>${row.firstName} ${row.lastName}</kyn-td>
+                  </kyn-tr>
+                `
+              )}
+            </kyn-tbody>
+          </kyn-table>
+        </div>
+      </kyn-meta-data>
+    `;
+  },
+};
+
+export const ScrollableContent = {
+  args: {
+    ...args,
+  },
+  parameters: {
+    a11y: {
+      disable: true,
+    },
+  },
+  render: (args) => {
+    return html`
+      <div style="max-width: 500px;">
+        <kyn-meta-data
+          ?horizontal=${args.horizontal}
+          ?noBackground=${args.noBackground}
+          scrollableContent
+          style="height: 150px;"
+        >
+          <div slot="label">Label</div>
+          <div
+            style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
+            class="example-content"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum
+          </div>
+        </kyn-meta-data>
+      </div>
+    `;
+  },
+};
+
+export const StaticGrid = {
+  args: {
+    ...args,
+    horizontal: true,
+  },
+  parameters: {
+    a11y: {
+      disable: true,
+    },
+  },
+  render: (args) => {
+    return html`
+      This example uses
+      <a
+        href="https://shidoka-foundation.netlify.app/?path=/docs/foundation-grid--docs"
+        target="_blank"
+        rel="noopener"
+      >
+        Shidoka Foundation Grid
+      </a>
+      for a static dashboard layout.
+      <br /><br />
+
+      <div class="kd-grid">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+      </div>
+
+      <div class="kd-grid" style="margin-top: 32px;">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-3">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div class="line_height">
+              ${Link.render({ standalone: true, ...Link.args })}
+            </div>
+          </kyn-meta-data>
+        </div>
+      </div>
+
+      <div class="kd-grid" style="margin-top: 32px;">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+            scrollableContent
+            style="height: 150px;"
+          >
+            <div slot="label">Label</div>
+            <div
+              style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
+              class="example-content"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum
+            </div>
+          </kyn-meta-data>
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+            scrollableContent
+            style="height: 150px;"
+          >
+            <div slot="label">Label</div>
+            <div
+              style="color: var(--kd-color-text-level-secondary);line-height: 18px;"
+              class="example-content"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum
+            </div>
+          </kyn-meta-data>
+        </div>
+      </div>
+
+      <div class="kd-grid" style="margin-top: 32px;">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-12">
+          <kyn-meta-data
+            ?horizontal=${args.horizontal}
+            ?noBackground=${args.noBackground}
+          >
+            <div slot="label">Label</div>
+            <div
+              class="line_height"
+              style="color: var(--kd-color-text-level-secondary);font-size: 14px;
+          border-radius: 4px;"
+            >
+              Mauris aliquet elementum dui, a mattis ligula feugiat eu. Ut
+              ullamcorper orci lacus, ac efficitur quam tincidunt sit amet.
+              Aenean magna ante, pretium non iaculis.
+            </div>
+          </kyn-meta-data>
+        </div>
+      </div>
+
+      <div class="kd-grid" style="margin-top: 32px;">
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          ${WithTable.render({
+            ...WithTable.args,
+            horizontal: args.horizontal,
+          })}
+        </div>
+
+        <div class="kd-grid__col--sm-4 kd-grid__col--md-4 kd-grid__col--lg-6">
+          ${WithTable.render({
+            ...WithTable.args,
+            horizontal: args.horizontal,
+          })}
+        </div>
+      </div>
+    `;
+  },
+};
---
+++ src/components/reusable/metaData/metaData.ts
@@ -0,0 +1,118 @@
+import { LitElement, html, unsafeCSS } from 'lit';
+import {
+  customElement,
+  property,
+  state,
+  queryAssignedElements,
+} from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import Styles from './metaData.scss?inline';
+
+export enum LABEL_POSITION {
+  TOP = 'top',
+  CENTER = 'center',
+  BOTTOM = 'bottom',
+}
+/**
+ * MetaData component.
+ * @slot icon - Slot for icon.
+ * @slot label - Slot for label.
+ * @slot unnamed - Slot for body/other content.
+ */
+@customElement('kyn-meta-data')
+export class MetaData extends LitElement {
+  static override styles = unsafeCSS(Styles);
+
+  /** Horizontal orientation. */
+  @property({ type: Boolean })
+  accessor horizontal = false;
+
+  /** No background. */
+  @property({ type: Boolean })
+  accessor noBackground = false;
+
+  /** Adds scrollable overflow to the slot content. */
+  @property({ type: Boolean })
+  accessor scrollableContent = false;
+
+  /** Determine the icon slot has content.
+   * @ignore
+   */
+  @queryAssignedElements({ slot: 'icon' })
+  accessor iconSlotItems!: Array<HTMLElement>;
+
+  /** Determine the icon slot has content.
+   * @ignore
+   */
+  @queryAssignedElements({ slot: 'label' })
+  accessor labelSlotItems!: Array<HTMLElement>;
+
+  /** Determine the icon slot has content.
+   * @ignore
+   */
+  @state()
+  private accessor hasIcon = false;
+
+  /** Determine the label slot has content.
+   * @internal
+   */
+  @state()
+  private accessor hasLabel = false;
+
+  override render() {
+    const metaContainer = {
+      'meta-container': true,
+      'no-background': this.noBackground,
+    };
+    const metaIcon = {
+      'meta-icon': true,
+      'horizontal-align': this.horizontal,
+      displayEle: !this.hasIcon,
+    };
+    const metaWrapper = {
+      'meta-wrapper': true,
+      'horizontal-align': this.horizontal,
+    };
+    const metaLabel = {
+      'meta-label': true,
+      displayEle: !this.hasLabel,
+    };
+    const metaValue = {
+      'meta-value': true,
+      scrollable: this.scrollableContent,
+    };
+    return html`
+      <div class="${classMap(metaContainer)}">
+        <div class="${classMap(metaIcon)}">
+          <slot name="icon" @slotchange=${() => this.onIconSlotChange()}></slot>
+        </div>
+        ${this.hasIcon ? html` <div class="spacer"></div> ` : null}
+        <div class="${classMap(metaWrapper)}">
+          <div class="${classMap(metaLabel)}">
+            <slot
+              name="label"
+              @slotchange=${() => this.onLabelSlotChange()}
+            ></slot>
+          </div>
+          <div class="${classMap(metaValue)}">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  private onIconSlotChange() {
+    this.hasIcon = this.iconSlotItems.length > 0;
+  }
+
+  private onLabelSlotChange() {
+    this.hasLabel = this.labelSlotItems.length > 0;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'kyn-meta-data': MetaData;
+  }
+}
