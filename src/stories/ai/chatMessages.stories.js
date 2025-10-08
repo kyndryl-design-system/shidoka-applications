1c9c51badfe639109818ff2b24e3ce0d3c3df525
Brian Patrick <brian.patrick3@kyndryl.com>
2025-10-08 08:57:17 -0500

    docs(ai-chat): add answers to messages pattern

--- src/stories/ai/chatMessages.stories.js
+++ src/stories/ai/chatMessages.stories.js
@@ -1,6 +1,7 @@
 import { html } from 'lit';
 import { User, AI } from './response.stories.js';
 import { Default as infoCard } from './infoCard.stories.js';
+import { Default as Answers } from './aiAnswers.stories.js';
 
 export default {
   title: 'AI/Patterns/Chat',
@@ -16,7 +17,8 @@ export const ChatMessages = {
   render: () => {
     return html`
       <div class="main-div">
-        ${infoCard.render()}${User.render()}${infoCard.render()} ${AI.render()}
+        ${infoCard.render()} ${User.render()} ${infoCard.render()}
+        ${AI.render()} ${Answers.render({})}
       </div>
 
       <style>
