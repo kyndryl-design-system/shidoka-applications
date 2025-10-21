3667980be3b36a79e95235d1d4a33dad5f463d61
wbarbee <will.clayton.barbee@gmail.com>
2025-10-21 11:05:15 -0500

    fix(header, widget): floating header, border-radius updates

--- src/components/global/uiShell/uiShell.stories.js
+++ src/components/global/uiShell/uiShell.stories.js
@@ -1,15 +1,18 @@
-import { unsafeSVG } from 'lit-html/directives/unsafe-svg.js';
 import { html } from 'lit';
 import './index';
 import '../footer';
 import '../header';
 import '../localNav';
 import '../../reusable/table';
-import allData from './../../reusable/table/story-helpers/table-data.json';
 
-import userAvatarIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/user.svg';
-import helpIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/20/question.svg';
-import sampleIcon from '@kyndryl-design-system/shidoka-icons/svg/monochrome/16/circle-stroke.svg';
+import { LocalNav } from '../localNav/localNav.stories.js';
+import {
+  Header,
+  WithEverything as HeaderWithEverything,
+} from '../header/Header.stories.js';
+import { WithLinks as FooterWithLinks } from '../footer/footer.stories';
+import { Basic as BasicTable } from '../../reusable/table/table.stories.js';
+import { PageTitle } from '../../reusable/pagetitle/pageTitle.stories.js';
 
 export default {
   title: 'Global Components/UI Shell',
@@ -31,16 +34,11 @@ export default {
 export const UIShell = {
   render: () => html`
     <kyn-ui-shell>
-      <kyn-header appTitle="UI Shell Example"></kyn-header>
+      ${Header.render({ ...Header.args })}
 
       <main>Main content here.</main>
 
-      <kyn-footer logoAriaLabel="Go to Homepage">
-        <span slot="copyright">
-          Copyright &copy; ${new Date().getFullYear()} Kyndryl Inc. All rights
-          reserved.
-        </span>
-      </kyn-footer>
+      ${FooterWithLinks.render({ ...FooterWithLinks.args })}
     </kyn-ui-shell>
   `,
 };
@@ -48,60 +46,12 @@ export const UIShell = {
 export const WithLocalNav = {
   render: () => html`
     <kyn-ui-shell>
-      <kyn-header appTitle="UI Shell Example"></kyn-header>
-
-      <kyn-local-nav>
-        <kyn-local-nav-link href="javascript:void(0)" active>
-          <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-          Link 1
-        </kyn-local-nav-link>
-
-        <kyn-local-nav-link href="javascript:void(0)">
-          <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-          Link 2
-
-          <kyn-local-nav-link slot="links" href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            L2 Link 1
-          </kyn-local-nav-link>
-          <kyn-local-nav-link slot="links" href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            L2 Link 2
-          </kyn-local-nav-link>
-        </kyn-local-nav-link>
-
-        <kyn-local-nav-link href="javascript:void(0)">
-          <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-          Link 3
-
-          <kyn-local-nav-link slot="links" href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            L2 Link 1
-
-            <kyn-local-nav-link slot="links" href="javascript:void(0)">
-              <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-              L3 Link 1
-            </kyn-local-nav-link>
-            <kyn-local-nav-link slot="links" href="javascript:void(0)">
-              <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-              L3 Link 2
-            </kyn-local-nav-link>
-          </kyn-local-nav-link>
-          <kyn-local-nav-link slot="links" href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            L2 Link 2
-          </kyn-local-nav-link>
-        </kyn-local-nav-link>
-      </kyn-local-nav>
+      ${Header.render({ ...Header.args })}
+      ${LocalNav.render({ ...LocalNav.args })}
 
       <main>Main content here.</main>
 
-      <kyn-footer logoAriaLabel="Go to Homepage">
-        <span slot="copyright">
-          Copyright &copy; ${new Date().getFullYear()} Kyndryl Inc. All rights
-          reserved.
-        </span>
-      </kyn-footer>
+      ${FooterWithLinks.render({ ...FooterWithLinks.args })}
     </kyn-ui-shell>
   `,
 };
@@ -113,184 +63,18 @@ export const WithEverything = {
     },
   },
   render: () => {
-    const data = allData.slice(0, 5);
-
     return html`
       <kyn-ui-shell>
-        <kyn-header appTitle="UI Shell Example">
-          <kyn-header-nav>
-            <kyn-header-link href="javascript:void(0)">
-              <span>${unsafeSVG(sampleIcon)}</span>
-              Link 1
-            </kyn-header-link>
-
-            <kyn-header-category heading="Category">
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Link 2
-              </kyn-header-link>
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Link 3
-              </kyn-header-link>
-            </kyn-header-category>
-
-            <kyn-header-divider></kyn-header-divider>
-
-            <kyn-header-link href="javascript:void(0)">
-              <span>${unsafeSVG(sampleIcon)}</span>
-              Link 4
-
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 1
-              </kyn-header-link>
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 2
-              </kyn-header-link>
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 3
-              </kyn-header-link>
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 4
-              </kyn-header-link>
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 5
-              </kyn-header-link>
-              <kyn-header-link slot="links" href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Sub Link 6
-              </kyn-header-link>
-            </kyn-header-link>
-          </kyn-header-nav>
-
-          <kyn-header-flyouts>
-            <kyn-header-flyout label="Menu Label">
-              <span slot="button">${unsafeSVG(helpIcon)}</span>
-
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Example 1
-              </kyn-header-link>
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Example 2
-              </kyn-header-link>
-            </kyn-header-flyout>
-
-            <kyn-header-flyout label="Menu Label" hideMenuLabel>
-              <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
-
-              <kyn-header-user-profile
-                name="User Name"
-                subtitle="Job Title"
-                email="user@kyndryl.com"
-                profileLink="#"
-              >
-                <img alt="" src="https://picsum.photos/id/237/112/112" />
-              </kyn-header-user-profile>
-
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Example Link 1
-              </kyn-header-link>
-              <kyn-header-link href="javascript:void(0)">
-                <span>${unsafeSVG(sampleIcon)}</span>
-                Example Link 2
-              </kyn-header-link>
-            </kyn-header-flyout>
-          </kyn-header-flyouts>
-        </kyn-header>
-
-        <kyn-local-nav>
-          <kyn-local-nav-link href="javascript:void(0)" active>
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            Link 1
-          </kyn-local-nav-link>
-
-          <kyn-local-nav-link href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            Link 2
-
-            <kyn-local-nav-link slot="links" href="javascript:void(0)">
-              <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-              L2 Link 1
-            </kyn-local-nav-link>
-            <kyn-local-nav-link
-              leftPadding
-              slot="links"
-              href="javascript:void(0)"
-            >
-              L2 Link 2
-            </kyn-local-nav-link>
-          </kyn-local-nav-link>
-
-          <kyn-local-nav-link href="javascript:void(0)">
-            <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-            Link 3
-
-            <kyn-local-nav-link slot="links" href="javascript:void(0)">
-              <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-              L2 Link 1
-
-              <kyn-local-nav-link slot="links" href="javascript:void(0)">
-                <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-                L3 Link 1
-              </kyn-local-nav-link>
-              <kyn-local-nav-link slot="links" href="javascript:void(0)">
-                <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-                L3 Link 2
-              </kyn-local-nav-link>
-            </kyn-local-nav-link>
-            <kyn-local-nav-link slot="links" href="javascript:void(0)">
-              <span slot="icon">${unsafeSVG(sampleIcon)}</span>
-              L2 Link 2
-            </kyn-local-nav-link>
-          </kyn-local-nav-link>
-        </kyn-local-nav>
+        ${HeaderWithEverything.render({ ...HeaderWithEverything.args })}
+        ${LocalNav.render({ ...LocalNav.args })}
 
         <main>
-          <h1 class="kd-type--headline-03">Example Content</h1>
-
-          <kyn-table-toolbar tableTitle="Basic Table"></kyn-table-toolbar>
-          <kyn-table-container>
-            <kyn-table>
-              <kyn-thead>
-                <kyn-tr>
-                  <kyn-th>ID</kyn-th>
-                  <kyn-th>First Name</kyn-th>
-                  <kyn-th>Last Name</kyn-th>
-                  <kyn-th>Birthday</kyn-th>
-                  <kyn-th .align=${'right'}>Age</kyn-th>
-                  <kyn-th>Full Name</kyn-th>
-                </kyn-tr>
-              </kyn-thead>
-              <kyn-tbody>
-                ${data.map(
-                  ({ id, firstName, lastName, birthday, age }) => html`<kyn-tr>
-                    <kyn-td>${id}</kyn-td>
-                    <kyn-td>${firstName}</kyn-td>
-                    <kyn-td>${lastName}</kyn-td>
-                    <kyn-td>${birthday}</kyn-td>
-                    <kyn-td .align=${'right'}>${age}</kyn-td>
-                    <kyn-td>${firstName} ${lastName}</kyn-td>
-                  </kyn-tr>`
-                )}
-              </kyn-tbody>
-            </kyn-table>
-          </kyn-table-container>
+          ${PageTitle.render({ ...PageTitle.args })}
+          <br />
+          ${BasicTable.render({ ...BasicTable.args })}
         </main>
 
-        <kyn-footer logoAriaLabel="Go to Homepage">
-          <span slot="copyright">
-            Copyright &copy; ${new Date().getFullYear()} Kyndryl Inc. All rights
-            reserved.
-          </span>
-        </kyn-footer>
+        ${FooterWithLinks.render({ ...FooterWithLinks.args })}
       </kyn-ui-shell>
     `;
   },
