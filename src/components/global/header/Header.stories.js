79d50493f62515d6cf6b8d09e7be8d13af660256
will-barbee-kyndryl <will.barbee@kyndryl.com>
2025-12-04 16:10:50 -0600

    fix: stories re-ordered, re-named
    Signed-off-by: will-barbee-kyndryl <will.barbee@kyndryl.com>

--- src/components/global/header/Header.stories.js
+++ src/components/global/header/Header.stories.js
@@ -174,6 +174,268 @@ export const WithNav = {
   `,
 };
 
+export const WithFlyouts = {
+  args,
+  render: (args) => html`
+    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
+      <kyn-header-flyouts>
+        <kyn-header-flyout label="Menu Label" hideButtonLabel>
+          <span slot="button">${unsafeSVG(helpIcon)}</span>
+          <span slot="button" title="Full Button Text">Short ... Text</span>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout label="Menu Label">
+          <span slot="button">${unsafeSVG(helpIcon)}</span>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout label="Menu Label" hideMenuLabel>
+          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
+
+          <kyn-header-user-profile
+            name="User Name"
+            subtitle="Job Title"
+            email="user@kyndryl.com"
+            profileLink="#"
+          >
+            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
+          </kyn-header-user-profile>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+      </kyn-header-flyouts>
+    </kyn-header>
+  `,
+};
+
+export const WithNotificationPanel = {
+  args,
+  render: (args) => html`
+    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
+      <kyn-header-flyouts>
+        <kyn-header-flyout label="Notification" hideMenuLabel>
+          <span slot="button">${unsafeSVG(filledNotificationIcon)}</span>
+          <kyn-header-notification-panel
+            panelTitle=${notificationPanelArgs.panelTitle}
+            panelFooterBtnText=${notificationPanelArgs.panelFooterBtnText}
+            ?hidePanelFooter=${notificationPanelArgs.hidePanelFooter}
+            @on-footer-btn-click=${(e) =>
+              action(e.type)({ ...e, detail: e.detail })}
+          >
+            <kyn-button
+              slot="menu-slot"
+              kind="secondary"
+              @click=${(e) => selectAllNotificationsAsRead(e)}
+            >
+              Mark all as Read
+            </kyn-button>
+            <kyn-button
+              slot="menu-slot"
+              kind="outline"
+              @click=${(e) => console.log(e)}
+            >
+              <span slot="icon">${unsafeSVG(settingsIcon)}</span>
+            </kyn-button>
+
+            ${notificationTagStatusArr.map(
+              (ele) => html`
+                <kyn-notification
+                  notificationTitle="Notification Title"
+                  notificationSubtitle="Application or Service"
+                  timeStamp="2 mins ago"
+                  href="#"
+                  type="clickable"
+                  tagStatus=${ele.tagStatus}
+                  unRead
+                  @on-notification-click=${(e) =>
+                    action(e.type)({ ...e, detail: e.detail })}
+                >
+                  <kyn-overflow-menu
+                    slot="actions"
+                    anchorRight
+                    @click=${(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                  >
+                    <kyn-overflow-menu-item
+                      @on-click=${(e) => handleOverflowClick(e)}
+                    >
+                      Mark as Read
+                    </kyn-overflow-menu-item>
+                    <kyn-overflow-menu-item>
+                      View Details
+                    </kyn-overflow-menu-item>
+                  </kyn-overflow-menu>
+
+                  <div>
+                    Message, this is an additional line Ipsum iMessage, Lorem
+                    Ipsum is simply dummy and typesetting industry.
+                  </div>
+                </kyn-notification>
+              `
+            )}
+          </kyn-header-notification-panel>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout label="Menu Label">
+          <span slot="button">${unsafeSVG(helpIcon)}</span>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout label="Menu Label" hideMenuLabel>
+          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
+
+          <kyn-header-user-profile
+            name="User Name"
+            subtitle="Job Title"
+            email="user@kyndryl.com"
+            profileLink="#"
+          >
+            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
+          </kyn-header-user-profile>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+      </kyn-header-flyouts>
+    </kyn-header>
+  `,
+};
+
+export const WithEverything = {
+  args,
+  render: (args) => html`
+    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
+      <kyn-header-nav>
+        <kyn-header-link href="javascript:void(0)">
+          <span>${unsafeSVG(circleIcon)}</span>
+          Link 1
+        </kyn-header-link>
+
+        <kyn-header-category heading="Category">
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Link 2
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Link 3
+          </kyn-header-link>
+        </kyn-header-category>
+
+        <kyn-header-divider></kyn-header-divider>
+
+        <kyn-header-link href="javascript:void(0)">
+          <span>${unsafeSVG(circleIcon)}</span>
+          Link 4
+
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 1
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 2
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 3
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 4
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 5
+          </kyn-header-link>
+          <kyn-header-link slot="links" href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Sub Link 6
+          </kyn-header-link>
+        </kyn-header-link>
+      </kyn-header-nav>
+
+      <kyn-button size="small">Button</kyn-button>
+
+      <kyn-header-flyouts>
+        <kyn-header-flyout label="Menu Label">
+          <span slot="button">${unsafeSVG(helpIcon)}</span>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+
+        <kyn-header-flyout label="Menu Label" hideMenuLabel>
+          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
+
+          <kyn-header-user-profile
+            name="User Name"
+            subtitle="Job Title"
+            email="user@kyndryl.com"
+            profileLink="#"
+          >
+            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
+          </kyn-header-user-profile>
+
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 1
+          </kyn-header-link>
+          <kyn-header-link href="javascript:void(0)">
+            <span>${unsafeSVG(circleIcon)}</span>
+            Example Link 2
+          </kyn-header-link>
+        </kyn-header-flyout>
+      </kyn-header-flyouts>
+    </kyn-header>
+  `,
+};
+
 // -----------------------------------------------------------------------------
 // JSON-driven categorized nav with configurable linkRenderer
 // -----------------------------------------------------------------------------
@@ -300,8 +562,7 @@ export const WithCategorizedNav = {
     `;
   },
 };
-
-WithCategorizedNav.storyName = 'With Categorized Nav (JSON-driven)';
+WithCategorizedNav.storyName = 'Categorical Nav (JSON-driven)';
 
 // -----------------------------------------------------------------------------
 // Fully manual HTML variant (no JSON) - slot driven
@@ -666,267 +927,4 @@ export const WithCategorizedNavManualHtml = {
     `;
   },
 };
-WithCategorizedNavManualHtml.storyName = 'With Categorized Nav (Manual HTML)';
-
-export const WithFlyouts = {
-  args,
-  render: (args) => html`
-    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
-      <kyn-header-flyouts>
-        <kyn-header-flyout label="Menu Label" hideButtonLabel>
-          <span slot="button">${unsafeSVG(helpIcon)}</span>
-          <span slot="button" title="Full Button Text">Short ... Text</span>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-
-        <kyn-header-flyout label="Menu Label">
-          <span slot="button">${unsafeSVG(helpIcon)}</span>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-
-        <kyn-header-flyout label="Menu Label" hideMenuLabel>
-          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
-
-          <kyn-header-user-profile
-            name="User Name"
-            subtitle="Job Title"
-            email="user@kyndryl.com"
-            profileLink="#"
-          >
-            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
-          </kyn-header-user-profile>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-      </kyn-header-flyouts>
-    </kyn-header>
-  `,
-};
-WithCategorizedNavManualHtml.storyName = 'With Categorized Nav (Manual HTML)';
-
-export const WithNotificationPanel = {
-  args,
-  render: (args) => html`
-    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
-      <kyn-header-flyouts>
-        <kyn-header-flyout label="Notification" hideMenuLabel>
-          <span slot="button">${unsafeSVG(filledNotificationIcon)}</span>
-          <kyn-header-notification-panel
-            panelTitle=${notificationPanelArgs.panelTitle}
-            panelFooterBtnText=${notificationPanelArgs.panelFooterBtnText}
-            ?hidePanelFooter=${notificationPanelArgs.hidePanelFooter}
-            @on-footer-btn-click=${(e) =>
-              action(e.type)({ ...e, detail: e.detail })}
-          >
-            <kyn-button
-              slot="menu-slot"
-              kind="secondary"
-              @click=${(e) => selectAllNotificationsAsRead(e)}
-            >
-              Mark all as Read
-            </kyn-button>
-            <kyn-button
-              slot="menu-slot"
-              kind="outline"
-              @click=${(e) => console.log(e)}
-            >
-              <span slot="icon">${unsafeSVG(settingsIcon)}</span>
-            </kyn-button>
-
-            ${notificationTagStatusArr.map(
-              (ele) => html`
-                <kyn-notification
-                  notificationTitle="Notification Title"
-                  notificationSubtitle="Application or Service"
-                  timeStamp="2 mins ago"
-                  href="#"
-                  type="clickable"
-                  tagStatus=${ele.tagStatus}
-                  unRead
-                  @on-notification-click=${(e) =>
-                    action(e.type)({ ...e, detail: e.detail })}
-                >
-                  <kyn-overflow-menu
-                    slot="actions"
-                    anchorRight
-                    @click=${(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                    }}
-                  >
-                    <kyn-overflow-menu-item
-                      @on-click=${(e) => handleOverflowClick(e)}
-                    >
-                      Mark as Read
-                    </kyn-overflow-menu-item>
-                    <kyn-overflow-menu-item>
-                      View Details
-                    </kyn-overflow-menu-item>
-                  </kyn-overflow-menu>
-
-                  <div>
-                    Message, this is an additional line Ipsum iMessage, Lorem
-                    Ipsum is simply dummy and typesetting industry.
-                  </div>
-                </kyn-notification>
-              `
-            )}
-          </kyn-header-notification-panel>
-        </kyn-header-flyout>
-
-        <kyn-header-flyout label="Menu Label">
-          <span slot="button">${unsafeSVG(helpIcon)}</span>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-
-        <kyn-header-flyout label="Menu Label" hideMenuLabel>
-          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
-
-          <kyn-header-user-profile
-            name="User Name"
-            subtitle="Job Title"
-            email="user@kyndryl.com"
-            profileLink="#"
-          >
-            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
-          </kyn-header-user-profile>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-      </kyn-header-flyouts>
-    </kyn-header>
-  `,
-};
-
-export const WithEverything = {
-  args,
-  render: (args) => html`
-    <kyn-header rootUrl=${args.rootUrl} appTitle=${args.appTitle}>
-      <kyn-header-nav>
-        <kyn-header-link href="javascript:void(0)">
-          <span>${unsafeSVG(circleIcon)}</span>
-          Link 1
-        </kyn-header-link>
-
-        <kyn-header-category heading="Category">
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Link 2
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Link 3
-          </kyn-header-link>
-        </kyn-header-category>
-
-        <kyn-header-divider></kyn-header-divider>
-
-        <kyn-header-link href="javascript:void(0)">
-          <span>${unsafeSVG(circleIcon)}</span>
-          Link 4
-
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 1
-          </kyn-header-link>
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 2
-          </kyn-header-link>
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 3
-          </kyn-header-link>
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 4
-          </kyn-header-link>
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 5
-          </kyn-header-link>
-          <kyn-header-link slot="links" href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Sub Link 6
-          </kyn-header-link>
-        </kyn-header-link>
-      </kyn-header-nav>
-
-      <kyn-button size="small">Button</kyn-button>
-
-      <kyn-header-flyouts>
-        <kyn-header-flyout label="Menu Label">
-          <span slot="button">${unsafeSVG(helpIcon)}</span>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-
-        <kyn-header-flyout label="Menu Label" hideMenuLabel>
-          <span slot="button">${unsafeSVG(userAvatarIcon)}</span>
-
-          <kyn-header-user-profile
-            name="User Name"
-            subtitle="Job Title"
-            email="user@kyndryl.com"
-            profileLink="#"
-          >
-            <img src="https://picsum.photos/id/237/112/112" alt="User Name" />
-          </kyn-header-user-profile>
-
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 1
-          </kyn-header-link>
-          <kyn-header-link href="javascript:void(0)">
-            <span>${unsafeSVG(circleIcon)}</span>
-            Example Link 2
-          </kyn-header-link>
-        </kyn-header-flyout>
-      </kyn-header-flyouts>
-    </kyn-header>
-  `,
-};
+WithCategorizedNavManualHtml.storyName = 'Categorical Nav (Slotted)';
