d4e504d7219d14c3f0aa331c6131a5cf16cd272d
will-barbee-kyndryl <will.barbee@kyndryl.com>
2026-06-02 08:46:11 -0500

    fix: update global switcher nav
    Signed-off-by: will-barbee-kyndryl <will.barbee@kyndryl.com>

---
+++ src/stories/globalSwitcher/globalSwitcherPatternStyles.js
@@ -0,0 +1,21 @@
+export const GLOBAL_SWITCHER_PATTERN_STYLES = `
+.global-switcher-nav {
+  --kyn-icon-selector-animate-selection: 1;
+  --kyn-icon-selector-only-visible-on-hover: 1;
+  --kyn-icon-selector-persist-when-checked: 1;
+  --kyn-header-nav-menu-width: 246px;
+  --kyn-header-nav-menu-padding: 12px;
+  --kyn-header-nav-menu-background: var(--kd-color-background-menu-state-default);
+  --kyn-header-nav-menu-border-radius: 8px;
+  --kyn-header-nav-menu-open-border-radius: 8px 0 0 8px;
+  --kyn-header-nav-menu-overflow: visible;
+  --kyn-header-nav-sync-flyout-height: 1;
+  --kyn-header-link-flyout-background: var(--kd-color-background-container-overlay-background);
+  --kyn-header-link-flyout-padding: 0 0 0 246px;
+  --kyn-header-link-wrapper-padding: 16px;
+  --kyn-header-link-wrapper-background: transparent;
+  --kyn-header-link-wrapper-border-radius: 0;
+  --kyn-header-link-single-categorical-width: min(calc(246px + 350px), calc(100vw - 16px));
+  --kyn-header-link-two-column-width: min(calc(246px + 2 * 350px + 32px), calc(100vw - 16px));
+}
+`;
